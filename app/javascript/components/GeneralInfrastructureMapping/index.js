607e852a4004202199aa1de73414ed6fdf30f8fb
Aparna Karve <akarve@redhat.com>
2018-02-09 16:34:37 -0800

    Added function - `renderField` to render the label and fields
    Used patternfly-react `Form` component to build the form

--- app/javascript/components/GeneralInfrastructureMapping/index.js
+++ app/javascript/components/GeneralInfrastructureMapping/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
+import { Form, Grid } from 'patternfly-react';
 
 class GeneralInfrastructureMappingContainer extends React.Component {
   constructor(props) {
@@ -10,17 +11,48 @@ class GeneralInfrastructureMappingContainer extends React.Component {
   render() {
     return (
       <form className="form-horizontal">
-        <Field name="name" label={__('Name')} component="input" type="text" />
+        <Field
+          name="name"
+          label={__('Name')}
+          component={renderField}
+          type="text"
+        />
         <Field
           name="description"
           label={__('Description')}
-          component="input"
+          component={renderField}
           type="textarea"
         />
       </form>
     );
   }
 }
+
+const renderField = ({
+  input,
+  label,
+  controlId,
+  type,
+  meta: { pristine, touched, error },
+  ...props
+}) => {
+  const formGroupProps = { key: { label }, controlId, ...props };
+
+  return (
+    <Form.FormGroup {...formGroupProps}>
+      <Grid.Col componentClass={Form.ControlLabel} sm={2}>
+        {label}
+      </Grid.Col>
+      <Grid.Col sm={9}>
+        <Form.FormControl
+          {...input}
+          type={type}
+          componentClass={type === 'text' ? undefined : type}
+        />
+      </Grid.Col>
+    </Form.FormGroup>
+  );
+};
 
 // Decorate with reduxForm(). It will read the initialValues prop provided by connect()
 const GeneralInfrastructureMappingForm = reduxForm({
