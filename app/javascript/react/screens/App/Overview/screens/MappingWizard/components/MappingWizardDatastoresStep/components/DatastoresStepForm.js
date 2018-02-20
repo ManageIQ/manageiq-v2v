6f6f1f961009ad85385f77171be347f4af926a03
Mike Ro <mikerodev@gmail.com>
2018-02-20 14:43:59 -0500

    Handle selecting DualPaneMapper list items

--- app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardDatastoresStep/components/DatastoresStepForm.js
+++ app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardDatastoresStep/components/DatastoresStepForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { bindMethods } from 'patternfly-react';
 
 import DualPaneMapper from '../../DualPaneMapper/DualPaneMapper';
 import DualPaneMapperList from '../../DualPaneMapper/DualPaneMapperList';
@@ -7,8 +8,45 @@ import DualPaneMapperCount from '../../DualPaneMapper/DualPaneMapperCount';
 import DualPaneMapperListItem from '../../DualPaneMapper/DualPaneMapperListItem';
 
 class DatastoresStepForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedSourceDatastores: [],
+      selectedTargetDatastore: null
+    };
+
+    bindMethods(this, ['selectSourceDatastore', 'selectTargetDatastore']);
+  }
+
+  selectSourceDatastore(sourceDatastore) {
+    this.setState(prevState => {
+      const isAlreadySelected = prevState.selectedSourceDatastores.some(
+        datastore => datastore.id === sourceDatastore.id
+      );
+      if (isAlreadySelected) {
+        return {
+          selectedSourceDatastores: prevState.selectedSourceDatastores.filter(
+            datastore => datastore.id !== sourceDatastore.id
+          )
+        };
+      }
+      return {
+        selectedSourceDatastores: [
+          ...prevState.selectedSourceDatastores,
+          sourceDatastore
+        ]
+      };
+    });
+  }
+
+  selectTargetDatastore(targetDatastore) {
+    this.setState(() => ({ selectedTargetDatastore: targetDatastore }));
+  }
+
   render() {
     const { sourceDatastores, targetDatastores } = this.props;
+    const { selectedSourceDatastores, selectedTargetDatastore } = this.state;
     return (
       <DualPaneMapper handleButtonClick={() => {}}>
         <DualPaneMapperList listTitle="Source Datastores">
@@ -16,14 +54,14 @@ class DatastoresStepForm extends React.Component {
             <DualPaneMapperListItem
               item={item}
               key={item.id}
-              // selected={
-              //   selectedSourceDatastores &&
-              //   selectedSourceDatastores.some(
-              //     sourceDatastore => sourceDatastore.id === item.id
-              //   )
-              // }
-              // handleClick={this.selectSourceDatastore}
-              // handleKeyPress={this.selectSourceCluster}
+              selected={
+                selectedSourceDatastores &&
+                selectedSourceDatastores.some(
+                  sourceDatastore => sourceDatastore.id === item.id
+                )
+              }
+              handleClick={this.selectSourceDatastore}
+              handleKeyPress={this.selectSourceCluster}
             />
           ))}
           {/* <DualPaneMapperCount
@@ -36,12 +74,12 @@ class DatastoresStepForm extends React.Component {
             <DualPaneMapperListItem
               item={item}
               key={item.id}
-              // selected={
-              //   selectedTargetDatastore &&
-              //   selectedTargetDatastore.id === item.id
-              // }
-              // handleClick={this.selectTargetDatastore}
-              // handleKeyPress={this.selectTargetDatastore}
+              selected={
+                selectedTargetDatastore &&
+                selectedTargetDatastore.id === item.id
+              }
+              handleClick={this.selectTargetDatastore}
+              handleKeyPress={this.selectTargetDatastore}
             />
           ))}
         </DualPaneMapperList>
