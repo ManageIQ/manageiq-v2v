1f0e6ade5252bfdaf7648ef34ae49b3402300e24
Mike Turley <mturley@redhat.com>
2018-02-21 15:58:30 -0500

    Clean up state

--- app/javascript/react/screens/App/Overview/screens/PlanWizard/components/PlanWizardCSVStep/PlanWizardCSVStep.js
+++ app/javascript/react/screens/App/Overview/screens/PlanWizard/components/PlanWizardCSVStep/PlanWizardCSVStep.js
@@ -13,16 +13,16 @@ class PlanWizardCSVStep extends React.Component {
   constructor() {
     super();
     bindMethods(this, ['onFileDrop']);
-    this.state = { fileUploaded: false, numItems: 0 };
+    this.state = { csvRows: [] };
   }
 
   onFileDrop(acceptedFiles, rejectedFiles) {
     if (acceptedFiles && acceptedFiles.length > 0) {
       const reader = new FileReader();
       reader.onload = () => {
-        csv.parse(reader.result, (err, data) => {
-          console.log('PARSED CSV DATA: ', data);
-          // TODO dispatch action to load data into redux
+        csv.parse(reader.result, (err, csvRows) => {
+          this.setState({ csvRows });
+          // TODO dispatch action to load data into redux instead of using this.state
         });
       };
       reader.readAsBinaryString(acceptedFiles[0]);
@@ -32,19 +32,36 @@ class PlanWizardCSVStep extends React.Component {
   }
 
   render() {
-    const { fileUploaded, numItems } = this.state;
-    const emptyDropzone = (
-      <EmptyState>
-        <EmptyState.Icon type="fa" name="upload"/>
-        <EmptyState.Title>Import a CSV file</EmptyState.Title>
-        <EmptyState.Info>
-          Drag-and-drop or click here to import a CSV file containing a list of VMs to be migrated.
+    const { csvRows } = this.state;
+    const dropzone = (
+      <Dropzone
+        ref={(node) => { dropzoneRef = node; }} // See comment at top for rationale...
+        onDrop={this.onFileDrop}
+        className={cx('csv-upload-dropzone')}
+        activeClassName={cx('active')}
+      >
+        <EmptyState>
+          <EmptyState.Icon type="fa" name="upload" />
+          <EmptyState.Title>Import a CSV file</EmptyState.Title>
+          <EmptyState.Info>
+            Drag-and-drop or click here to import a CSV file containing a list of VMs to be migrated.
         </EmptyState.Info>
-      </EmptyState>
+        </EmptyState>
+      </Dropzone>
     );
-    const fullDropzone = (
+    // TODO add a "back / upload a new file" button above the list
+    const droppedRows = (
       <ListView>
-        <ListView.Item>Look at me, I'm a VM!</ListView.Item> {/* TODO actually populate this */}
+        {csvRows && csvRows.length > 0 && csvRows.map(row => (
+          <ListView.Item
+            checkboxInput={<input type="checkbox" />}
+            heading={row[0] || ''}
+            description={row[1] || ''}
+            additionalInfo={row.length > 2 && row.slice(2).map(col => (
+              <ListView.InfoItem>{col}</ListView.InfoItem>
+            ))}
+          />
+        ))}
       </ListView>
     );
     return (
@@ -60,16 +77,9 @@ class PlanWizardCSVStep extends React.Component {
               <Icon type="fa" name="upload" /> Import
             </Button>
           </Toolbar.RightContent>
-          <Dropzone
-            ref={(node) => { dropzoneRef = node; }} // See comment at top for rationale...
-            onDrop={this.onFileDrop}
-            className={cx('csv-upload-dropzone')}
-            activeClassName={cx('active')}
-          >
-            {fileUploaded ? fullDropzone : emptyDropzone}
-          </Dropzone>
+          {csvRows && csvRows.length > 0 ? droppedRows : dropzone}
           <Toolbar.Results>
-            {numItems} Items
+            {csvRows && csvRows.length || 0} Items
           </Toolbar.Results>
         </Toolbar>
       </div>
