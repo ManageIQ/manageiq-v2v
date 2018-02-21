5187d9a4481ee5c8714af63d5dfdd47ad74a6cdb
Mike Ro <mikerodev@gmail.com>
2018-02-20 22:35:52 -0500

    Fix test errors

--- app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardDatastoresStep/components/SourceClusterSelect/SourceClusterSelect.js
+++ app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardDatastoresStep/components/SourceClusterSelect/SourceClusterSelect.js
@@ -16,47 +16,51 @@ const SourceClusterSelect = ({
   sourceClusters,
   selectSourceCluster,
   selectedCluster
-}) => {
-  return (
-    <div className="source-cluster-select">
-      <Form horizontal>
-        <FormGroup>
-          <Grid.Col componentClass={ControlLabel} sm={6}>
-            Map source datastores to target datastores for cluster
-          </Grid.Col>
-          <Grid.Col sm={3}>
-            <InputGroup>
-              <FormControl.Static>
-                {selectedCluster && selectedCluster.name ? (
-                  selectedCluster.name
-                ) : (
-                  <span className="placeholder-text">
-                    Select a source cluster
-                  </span>
-                )}
-              </FormControl.Static>
-              <DropdownButton
-                id="cluster-select"
-                title=""
-                componentClass={InputGroup.Button}
-                pullRight
-              >
-                {sourceClusters &&
-                  sourceClusters.map(cluster => (
-                    <MenuItem
-                      onSelect={() => selectSourceCluster(cluster.id)}
-                      key={cluster.id}
-                    >
-                      {cluster.name}
-                    </MenuItem>
-                  ))}
-              </DropdownButton>
-            </InputGroup>
-          </Grid.Col>
-        </FormGroup>
-      </Form>
-    </div>
-  );
-};
+}) => (
+  <div className="source-cluster-select">
+    <Form horizontal>
+      <FormGroup>
+        <Grid.Col componentClass={ControlLabel} sm={6}>
+          Map source datastores to target datastores for cluster
+        </Grid.Col>
+        <Grid.Col sm={3}>
+          <InputGroup>
+            <FormControl.Static>
+              {selectedCluster && selectedCluster.name ? (
+                selectedCluster.name
+              ) : (
+                <span className="placeholder-text">
+                  Select a source cluster
+                </span>
+              )}
+            </FormControl.Static>
+            <DropdownButton
+              id="cluster-select"
+              title=""
+              componentClass={InputGroup.Button}
+              pullRight
+            >
+              {sourceClusters &&
+                sourceClusters.map(cluster => (
+                  <MenuItem
+                    onSelect={() => selectSourceCluster(cluster.id)}
+                    key={cluster.id}
+                  >
+                    {cluster.name}
+                  </MenuItem>
+                ))}
+            </DropdownButton>
+          </InputGroup>
+        </Grid.Col>
+      </FormGroup>
+    </Form>
+  </div>
+);
 
 export default SourceClusterSelect;
+
+SourceClusterSelect.propTypes = {
+  sourceClusters: PropTypes.array,
+  selectSourceCluster: PropTypes.func,
+  selectedCluster: PropTypes.object
+};
