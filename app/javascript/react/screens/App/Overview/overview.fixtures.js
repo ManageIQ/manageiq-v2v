d3f56f365aa186c2e5b5e15634dd61fc16f309fe
Patrick Riley <priley86@gmail.com>
2018-02-22 14:32:52 -0500

    update fixtures

--- app/javascript/react/screens/App/Overview/overview.fixtures.js
+++ app/javascript/react/screens/App/Overview/overview.fixtures.js
@@ -13,16 +13,73 @@ export const initialState = Immutable({
 
 export const transformationMappings = [
   {
-    name: 'new transformation mapping',
-    description: 'clusters to clusters',
+    id: '10000000000001',
+    name: 'Infrastructre Mapping 1',
+    description: 'infrastructre mapping 1 description',
+    comments: null,
     state: 'draft',
+    options: {},
+    tenant_id: null,
+    created_at: '2018-02-07T16:02:39Z',
+    updated_at: '2018-02-07T16:02:39Z',
     transformation_mapping_items: [
-      { source: '/api/clusters/10', destination: '/api/clusters/1' },
-      { source: '/api/clusters/11', destination: '/api/clusters/1' },
-      { source: '/api/date_stores/12', destination: '/api/data_stores/13' },
-      { source: '/api/data_stores/2', destination: '/api/data_stores/13' },
-      { source: '/api/lans/2', destination: '/api/lans/13' },
-      { source: '/api/lans/3', destination: '/api/lans/13' }
+      {
+        id: '10000000000001',
+        source_id: '10000000000002',
+        source_type: 'EmsCluster',
+        destination_id: '10000000000005',
+        destination_type: 'EmsCluster',
+        transformation_mapping_id: '10000000000001',
+        options: {},
+        created_at: '2018-02-07T16:01:44Z',
+        updated_at: '2018-02-07T16:02:39Z'
+      },
+      {
+        id: '10000000000002',
+        source_id: '10000000000001',
+        source_type: 'Storage',
+        destination_id: '10000000000018',
+        destination_type: 'Storage',
+        transformation_mapping_id: '10000000000002',
+        options: {},
+        created_at: '2018-02-07T16:05:04Z',
+        updated_at: '2018-02-07T16:05:04Z'
+      }
+    ]
+  },
+  {
+    id: '10000000000002',
+    name: 'Infrastructure Mapping 2',
+    description: 'infrastructre mapping 2 description',
+    comments: null,
+    state: 'draft',
+    options: {},
+    tenant_id: null,
+    created_at: '2018-02-07T16:02:39Z',
+    updated_at: '2018-02-07T16:02:39Z',
+    transformation_mapping_items: [
+      {
+        id: '10000000000003',
+        source_id: '10000000000003',
+        source_type: 'EmsCluster',
+        destination_id: '10000000000005',
+        destination_type: 'EmsCluster',
+        transformation_mapping_id: '10000000000002',
+        options: {},
+        created_at: '2018-02-07T16:01:44Z',
+        updated_at: '2018-02-07T16:02:39Z'
+      },
+      {
+        id: '10000000000004',
+        source_id: '10000000000004',
+        source_type: 'Storage',
+        destination_id: '10000000000019',
+        destination_type: 'Storage',
+        transformation_mapping_id: '10000000000002',
+        options: {},
+        created_at: '2018-02-07T16:05:04Z',
+        updated_at: '2018-02-07T16:05:04Z'
+      }
     ]
   }
 ];
