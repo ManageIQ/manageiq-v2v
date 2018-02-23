84c845770171720886034a66d38c15f8eaf99d41
Aparna Karve <akarve@redhat.com>
2018-02-22 16:39:20 -0800

    Add `select` functionality to FormField

--- app/javascript/react/screens/App/common/forms/FormField.js
+++ app/javascript/react/screens/App/common/forms/FormField.js
@@ -8,12 +8,43 @@ export const FormField = ({
   required,
   controlId,
   type,
+  options,
+  optionKey,
+  optionValue,
   meta: { touched, error },
   ...props
 }) => {
   const formGroupProps = { key: { label }, controlId, ...props };
 
   if (touched && error) formGroupProps.validationState = 'error';
+
+  const renderField = () => {
+    let field;
+    switch (type) {
+      case 'textarea':
+        field = (
+          <Form.FormControl {...input} type={type} componentClass="textarea" />
+        );
+        break;
+      case 'text':
+        field = <Form.FormControl {...input} type={type} />;
+        break;
+      case 'select':
+        field = (
+          <select className="form-control" {...input}>
+            <option disabled value="">{`<${__('Choose')}>`}</option>
+            {options.map(val => (
+              <option value={val[optionKey]} key={val[optionValue]}>
+                {val[optionValue]}
+              </option>
+            ))}
+          </select>
+        );
+        break;
+      default:
+    }
+    return field;
+  };
 
   return (
     <Form.FormGroup {...formGroupProps}>
@@ -22,11 +53,7 @@ export const FormField = ({
         {required && ' *'}
       </Grid.Col>
       <Grid.Col sm={9}>
-        <Form.FormControl
-          {...input}
-          type={type}
-          componentClass={type === 'text' ? undefined : type}
-        />
+        {renderField()}
         {touched && error && <Form.HelpBlock>{error}</Form.HelpBlock>}
       </Grid.Col>
     </Form.FormGroup>
@@ -39,5 +66,8 @@ FormField.propTypes = {
   required: PropTypes.bool,
   controlId: PropTypes.string,
   type: PropTypes.string,
+  options: PropTypes.array,
+  optionKey: PropTypes.string,
+  optionValue: PropTypes.string,
   meta: PropTypes.object
 };
