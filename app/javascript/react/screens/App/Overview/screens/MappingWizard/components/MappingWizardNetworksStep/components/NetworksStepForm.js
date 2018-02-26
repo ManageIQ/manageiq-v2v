00b6c248a3a68bedddca9c9126b935b4a61a2b47
Mike Ro <mikerodev@gmail.com>
2018-02-26 10:50:25 -0500

    Handle adding network mappings
    Note:  As of now, cannot implement sourceNetworksFilter in
    mapStateToProps as redux-form has not initialized the new form yet.
    Specifically, we need to access
    
    form.mappingWizardNetworksStep
    
    in the store, but it is returning undefined

--- app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardNetworksStep/components/NetworksStepForm.js
+++ app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardNetworksStep/components/NetworksStepForm.js
@@ -8,6 +8,8 @@ import DualPaneMapperCount from '../../DualPaneMapper/DualPaneMapperCount';
 import DualPaneMapperListItem from '../../DualPaneMapper/DualPaneMapperListItem';
 // import DatastoresStepTreeView from './DatastoresStepTreeView';
 
+import { sourceNetworksFilter } from '../MappingWizardNetworksStepSelectors';
+
 class NetworksStepForm extends React.Component {
   constructor(props) {
     super(props);
@@ -17,7 +19,11 @@ class NetworksStepForm extends React.Component {
       selectedTargetNetwork: null
     };
 
-    bindMethods(this, ['selectSourceNetwork', 'selectTargetNetwork']);
+    bindMethods(this, [
+      'selectSourceNetwork',
+      'selectTargetNetwork',
+      'addNetworkMapping'
+    ]);
   }
 
   selectSourceNetwork(sourceNetwork) {
@@ -46,6 +52,122 @@ class NetworksStepForm extends React.Component {
     this.setState(() => ({ selectedTargetNetwork: targetNetwork }));
   }
 
+  addNetworkMapping() {
+    const { input, selectedClusterMapping } = this.props;
+
+    this.setState(prevState => {
+      const networksStepMappings = input.value;
+
+      const noMappingForTargetCluster = !networksStepMappings.some(
+        targetClusterWithNetworkMappings =>
+          // targetClusterWithNetworkMappings: { ...targetCluster, nodes: [...networkMappings] }
+          // selectedClusterMapping: { ...targetCluster, nodes: [...sourceClusters (one of which is currently selected)]}
+          targetClusterWithNetworkMappings.id === selectedClusterMapping.id
+      );
+
+      const addingToExistingMapping = networksStepMappings.some(
+        targetClusterWithNetworkMappings =>
+          targetClusterWithNetworkMappings.nodes.some(
+            networkMapping =>
+              // networkMapping: { ...targetNetwork, nodes: [...sourceNetworks] }
+              networkMapping.id === prevState.selectedTargetNetwork.id
+          )
+      );
+
+      if (networksStepMappings.length === 0 || noMappingForTargetCluster) {
+        // There are no mappings, or there does not already exist a node for
+        // a mapped target cluster
+        input.onChange([
+          ...networksStepMappings,
+          {
+            ...selectedClusterMapping,
+            text: selectedClusterMapping.name,
+            selectable: false,
+            nodes: [
+              {
+                ...prevState.selectedTargetNetwork,
+                text: prevState.selectedTargetNetwork.name,
+                selectable: true,
+                selected: false,
+                state: {
+                  expanded: true
+                },
+                nodes: prevState.selectedSourceNetworks.map(network => ({
+                  ...network,
+                  text: network.name,
+                  icon: 'fa fa-file-o'
+                }))
+              }
+            ]
+          }
+        ]);
+      } else {
+        input.onChange(
+          networksStepMappings.map(targetClusterWithNetworkMappings => {
+            // We are not adding a network mapping to this target cluster
+            if (
+              targetClusterWithNetworkMappings.id !== selectedClusterMapping.id
+            ) {
+              return targetClusterWithNetworkMappings;
+              // There already exists a mapping for the currently selected target
+              // network and we need to append the currently selected source networks
+              // to its source networks array
+            } else if (addingToExistingMapping) {
+              return {
+                ...targetClusterWithNetworkMappings,
+                nodes: targetClusterWithNetworkMappings.nodes.map(
+                  networkMapping => {
+                    // networkMapping: { ...targetNetwork, nodes: [...sourceNetworks] }
+                    // If the target network for this mapping matches the currently
+                    // selected target network, add the currently selected source
+                    // networks to its source networks array
+                    if (
+                      networkMapping.id === prevState.selectedTargetNetwork.id
+                    ) {
+                      return {
+                        ...networkMapping,
+                        nodes: networkMapping.nodes.concat(
+                          prevState.selectedSourceNetworks.map(network => ({
+                            ...network,
+                            text: network.name,
+                            icon: 'fa fa-file-o'
+                          }))
+                        )
+                      };
+                    }
+                    return networkMapping;
+                  }
+                )
+              };
+            }
+            // We are creating a new mapping to add under the target cluster
+            return {
+              ...targetClusterWithNetworkMappings,
+              nodes: targetClusterWithNetworkMappings.nodes.concat({
+                ...prevState.selectedTargetNetwork,
+                text: prevState.selectedTargetNetwork.name,
+                selectable: true,
+                selected: false,
+                state: {
+                  expanded: true
+                },
+                nodes: prevState.selectedSourceNetworks.map(network => ({
+                  ...network,
+                  text: network.name,
+                  icon: 'fa fa-file-o'
+                }))
+              })
+            };
+          })
+        );
+      }
+      return {
+        selectedTargetNetwork: null,
+        selectedSourceNetworks: []
+      };
+    });
+  }
+
   render() {
     const {
       sourceNetworks,
@@ -63,34 +185,36 @@ class NetworksStepForm extends React.Component {
     return (
       <div className="dual-pane-mapper-form">
         <DualPaneMapper
-        // handleButtonClick={this.addDatastoreMapping}
-        // validMapping={
-        //   !(
-        //     selectedTargetDatastore &&
-        //     (selectedSourceDatastores && selectedSourceDatastores.length > 0)
-        //   )
-        // }
+          handleButtonClick={this.addNetworkMapping}
+          validMapping={
+            !(
+              selectedTargetNetwork &&
+              (selectedSourceNetworks && selectedSourceNetworks.length > 0)
+            )
+          }
         >
           <DualPaneMapperList
             listTitle="Source Networks"
             loading={isFetchingSourceNetworks}
           >
             {sourceNetworks &&
-              sourceNetworks.map(sourceNetwork => (
-                <DualPaneMapperListItem
-                  item={sourceNetwork}
-                  key={sourceNetwork.id}
-                  selected={
-                    selectedSourceNetworks &&
-                    selectedSourceNetworks.some(
-                      selectedSourceNetwork =>
-                        selectedSourceNetwork.id === sourceNetwork.id
-                    )
-                  }
-                  handleClick={this.selectSourceNetwork}
-                  handleKeyPress={this.selectSourceNetwork}
-                />
-              ))}
+              sourceNetworksFilter(sourceNetworks, input.value).map(
+                sourceNetwork => (
+                  <DualPaneMapperListItem
+                    item={sourceNetwork}
+                    key={sourceNetwork.id}
+                    selected={
+                      selectedSourceNetworks &&
+                      selectedSourceNetworks.some(
+                        selectedSourceNetwork =>
+                          selectedSourceNetwork.id === sourceNetwork.id
+                      )
+                    }
+                    handleClick={this.selectSourceNetwork}
+                    handleKeyPress={this.selectSourceNetwork}
+                  />
+                )
+              )}
             {/* <DualPaneMapperCount
               selectedItems={selectedSourceDatastores.length}
               totalItems={sourceDatastores.length}
