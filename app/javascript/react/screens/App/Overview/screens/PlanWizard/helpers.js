b27400a9a677a615b4fee9c62756faa03ce6c671
Aparna Karve <akarve@redhat.com>
2018-02-28 13:53:41 -0800

    Construct the POST object with `createMigrationPlans`
    call it when activeStepIndex = 1
    
    Use PlanWizardSelectors to get redux forms for Step1 and Step2

---
+++ app/javascript/react/screens/App/Overview/screens/PlanWizard/helpers.js
@@ -0,0 +1,27 @@
+import { map, trim } from 'lodash';
+
+export const createMigrationPlans = (
+  planWizardGeneralStep,
+  planWizardCSVStep
+) => {
+  const planName = planWizardGeneralStep.values.name;
+  const planDescription = planWizardGeneralStep.values.description;
+  const infrastructureMapping =
+    planWizardGeneralStep.values.infrastructure_mapping;
+  const vms = map(planWizardCSVStep.values.csvRows, 'reference').map(trim);
+
+  return {
+    name: planName,
+    description: planDescription,
+    service_type: 'atomic',
+    prov_type: 'generic',
+    type: 'ServiceTemplateTransformationPlan',
+    display: false,
+    options: {
+      config_info: {
+        transformation_mapping_id: infrastructureMapping,
+        vms: vms
+      }
+    }
+  };
+};
