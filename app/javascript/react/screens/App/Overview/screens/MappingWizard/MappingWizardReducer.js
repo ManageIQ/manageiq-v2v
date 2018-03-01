f3288b083fcee2683a5bb42a3875b09b0372fa67
Mike Ro <mikerodev@gmail.com>
2018-02-28 20:58:19 -0500

    Set up action generators and reducers

--- app/javascript/react/screens/App/Overview/screens/MappingWizard/MappingWizardReducer.js
+++ app/javascript/react/screens/App/Overview/screens/MappingWizard/MappingWizardReducer.js
@@ -1,16 +1,28 @@
 import Immutable from 'seamless-immutable';
 
-import { V2V_SET_TRANSFORMATIONS_BODY } from './MappingWizardConstants';
+import {
+  V2V_SET_TRANSFORMATIONS_BODY,
+  V2V_SHOW_WARNING_MODAL,
+  V2V_HIDE_WARNING_MODAL
+} from './MappingWizardConstants';
 
 const initialState = Immutable({
-  transformationsBody: {}
+  transformationsBody: {},
+  warningModalVisible: false
 });
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case V2V_SET_TRANSFORMATIONS_BODY:
       return state.set('transformationsBody', action.payload);
-
+    case V2V_SHOW_WARNING_MODAL:
+      return Immutable.merge(state, {
+        warningModalVisible: true
+      });
+    case V2V_HIDE_WARNING_MODAL:
+      return Immutable.merge(state, {
+        warningModalVisible: false
+      });
     default:
       return state;
   }
