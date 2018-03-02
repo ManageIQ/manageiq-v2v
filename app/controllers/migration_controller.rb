53b0778175735551c8de32a500518c48b3b6d3db
Martin Hradil <mhradil@redhat.com>
2018-03-02 11:26:55 +0000

    MigrationController - set @layout
    needed for `item_nav_class` to find the active menu item

--- app/controllers/migration_controller.rb
+++ app/controllers/migration_controller.rb
@@ -1,7 +1,15 @@
 # Migration controller - mostly service to initialize the react top level component
 # and to handle the initial routing endpoint.
 class MigrationController < ApplicationController
-  def index; end
+  def index
+    # this sets the active menu item, must match the item name in lib/miq_v2v_ui/engine.rb
+    @layout = case params[:page]
+              when 'infrastructure-mappings'
+                'infrastructure_mappings'
+              else
+                'overview'
+              end
+  end
 
   menu_section :migration
 end
