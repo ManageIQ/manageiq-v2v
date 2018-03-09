5928de8ebdb7fb5726901b8a10ebebdd3dc3ba02
Mike Ro <mikerodev@gmail.com>
2018-03-09 13:10:04 -0500

    Fix linting errors

--- app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardGeneralStep/helpers.js
+++ app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardGeneralStep/helpers.js
@@ -1,25 +1,25 @@
-export const asyncValidate = (values, dispatch, props) => {
-  return new Promise(resolve => {
+export const asyncValidate = (values, dispatch, props) =>
+  new Promise(resolve => {
     resolve();
   }).then(() => {
     const { name: newTransformationName } = values;
     const { transformationMappings } = props;
     const existingTransformationNames = transformationMappings.reduce(
-      (names, transformationMapping) => {
-        return [...names, transformationMapping.name.trim()];
-      },
+      (names, transformationMapping) => [
+        ...names,
+        transformationMapping.name.trim()
+      ],
       []
     );
     const duplicateName = existingTransformationNames.find(
-      existingTransformationName => {
-        return existingTransformationName === newTransformationName.trim();
-      }
+      existingTransformationName =>
+        existingTransformationName === newTransformationName.trim()
     );
     if (duplicateName) {
-      throw { name: 'Please enter a unique name' };
+      const err = { name: 'Please enter a unique name' };
+      throw err;
     }
   });
-};
 
 export const onChange = (values, dispatch, props) => {
   if (props.valid) {
