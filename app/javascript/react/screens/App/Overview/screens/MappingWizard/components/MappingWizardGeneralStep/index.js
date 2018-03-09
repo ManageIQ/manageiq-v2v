b8a44251d2c0d0759bd90f036182547dcb25e9be
Mike Ro <mikerodev@gmail.com>
2018-03-09 12:47:07 -0500

    Validate infrastructure mapping name
    * Use redux-form async validation to check whether the currently entered
    name is a duplicate
    * If a duplicate is found, show alert, otherwise move forward to next
    wizard step
    * Automatically dismiss alert when user changes name

--- app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardGeneralStep/index.js
+++ app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardGeneralStep/index.js
@@ -1,12 +1,24 @@
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
 import MappingWizardGeneralStep from './MappingWizardGeneralStep';
+import { hideAlertAction } from '../../MappingWizardActions';
+
+import { asyncValidate, onChange } from './helpers';
 
 // Decorate with reduxForm(). It will read the initialValues prop provided by connect()
 const MappingWizardGeneralStepForm = reduxForm({
   form: 'mappingWizardGeneralStep', // a unique identifier for this form
-  destroyOnUnmount: false // preserve form data
+  destroyOnUnmount: false, // preserve form data
+  asyncValidate,
+  asyncBlurFields: ['name'],
+  onChange
   // forceUnregisterOnUnmount: true, // unregister fields on unmount
 })(MappingWizardGeneralStep);
 
-export default connect()(MappingWizardGeneralStepForm);
+const mapStateToProps = ({ overview: { transformationMappings } }) => ({
+  transformationMappings
+});
+
+const actions = { hideAlertAction };
+
+export default connect(mapStateToProps, actions)(MappingWizardGeneralStepForm);
