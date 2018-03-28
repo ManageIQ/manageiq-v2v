850d79e9dc8246f02331a3e7d4e30dd08ada7843
Aparna Karve <akarve@redhat.com>
2018-03-28 13:33:59 -0700

    Set the default choice of VM selection to `vms_via_discovery`

--- app/javascript/react/screens/App/Overview/screens/PlanWizard/components/PlanWizardGeneralStep/index.js
+++ app/javascript/react/screens/App/Overview/screens/PlanWizard/components/PlanWizardGeneralStep/index.js
@@ -3,7 +3,10 @@ import PlanWizardGeneralStep from './PlanWizardGeneralStep';
 
 const mapStateToProps = ({ overview }) => ({
   transformationMappings: overview.transformationMappings,
-  initialValues: { infrastructure_mapping: overview.planWizardId }
+  initialValues: {
+    infrastructure_mapping: overview.planWizardId,
+    vm_choice_radio: 'vms_via_discovery'
+  }
 });
 
 export default connect(mapStateToProps)(PlanWizardGeneralStep);
