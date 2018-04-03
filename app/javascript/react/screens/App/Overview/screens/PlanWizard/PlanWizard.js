a393b08d0197300bcafa9bdfc8069258f609e983
Mike Turley <mturley@redhat.com>
2018-04-02 22:58:30 -0400

    Pull discovery mode into PlanWizard's nextStep logic for API requests

--- app/javascript/react/screens/App/Overview/screens/PlanWizard/PlanWizard.js
+++ app/javascript/react/screens/App/Overview/screens/PlanWizard/PlanWizard.js
@@ -40,18 +40,20 @@ class PlanWizard extends React.Component {
       setPlanScheduleAction
     } = this.props;
 
+    const { vm_choice_radio } = planWizardGeneralStep.values || {};
+    const discoveryMode = vm_choice_radio === 'vms_via_discovery';
+
     // NOTE/TODO: This is special logic that is not present in the refactored wizard abstraction!
     // MTURLEY: Make sure to incorporate this in that rebase!
     if (activeStepIndex === 0) {
-      if (!'in CSV mode') {
+      if (discoveryMode) {
         // TODO this boolean?
         // TODO: request discovery of VMs
         console.log('TODO: API REQUEST FOR VM DISCOVERY HERE');
       }
     }
-
     if (activeStepIndex === 2) {
-      if ('in CSV mode') {
+      if (!discoveryMode) {
         // TODO this boolean?
         // TODO: request validation of CSV VM list
         console.log('TODO: API REQUEST FOR VM CSV VALIDATION HERE');
