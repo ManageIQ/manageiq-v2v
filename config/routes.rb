22ac575d5f6b33b1e237a2110db83f7da9f8595b
Aparna Karve <akarve@redhat.com>
2018-04-09 13:02:23 -0700

    Support Migration routes if ::Settings.product.transformation is true
    https://bugzilla.redhat.com/show_bug.cgi?id=1565328

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # MiqV2vUI::Engine.routes.draw do
-Rails.application.routes.draw do
-  match '/migration' => 'migration#index', :via => [:get]
-  match 'migration/*page' => 'migration#index', :via => [:get]
+if ::Settings.product.transformation == true
+  Rails.application.routes.draw do
+    match '/migration' => 'migration#index', :via => [:get]
+    match 'migration/*page' => 'migration#index', :via => [:get]
+  end
 end
