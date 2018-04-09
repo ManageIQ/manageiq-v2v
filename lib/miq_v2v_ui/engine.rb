f59d6f529817724e5940dd0b20dc7bdc3bfbfbec
Aparna Karve <akarve@redhat.com>
2018-04-09 13:01:52 -0700

    Render Migration Menu if ::Settings.product.transformation is true

--- lib/miq_v2v_ui/engine.rb
+++ lib/miq_v2v_ui/engine.rb
@@ -10,13 +10,15 @@ module MiqV2vUI
       app.config.assets.paths << root.join('assets', 'images').to_s
     end
 
-    initializer 'plugin' do
-      Menu::CustomLoader.register(
-        Menu::Section.new(:migration, N_("Migration"), 'fa fa-plus', [
+    def renderMigrationMenu
+      Menu::Section.new(:migration, N_("Migration"), 'fa fa-plus', [
           Menu::Item.new('overview', N_('Overview'), 'miq_report', {:feature => 'miq_report', :any => true}, '/migration'),
           Menu::Item.new('infrastructure_mappings', N_('Infrastructure Mappings'), 'miq_report', {:feature => 'miq_report', :any => true}, '/migration/infrastructure-mappings')
-        ], nil, nil, nil, nil, :compute),
-      )
+      ], nil, nil, nil, nil, :compute)
+    end
+
+    initializer 'plugin' do
+      Menu::CustomLoader.register(::Settings.product.transformation == true ? renderMigrationMenu : nil)
     end
   end
 end
