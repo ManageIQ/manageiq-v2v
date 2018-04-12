71bb34601924a51b28a7424f94ae3066f8709e7f
Patrick Riley <priley86@gmail.com>
2018-04-12 09:37:25 -0400

    fixed polling reset

--- app/javascript/react/screens/App/Plan/PlanActions.js
+++ app/javascript/react/screens/App/Plan/PlanActions.js
@@ -7,18 +7,20 @@ import { requestPlanRequestsData } from './plan.fixtures';
 
 const mockMode = globalMockMode;
 
-const _getPlanRequestsActionCreator = url => dispatch =>
-  dispatch({
-    type: FETCH_V2V_PLAN_REQUESTS,
-    payload: API.get(url)
-  }).catch(error => {
-    if (mockMode) {
-      dispatch({
-        type: `${FETCH_V2V_PLAN_REQUESTS}_FULFILLED`,
-        payload: requestPlanRequestsData.response
-      });
-    }
-  });
+const _getPlanRequestsActionCreator = url => dispatch => {
+  if (mockMode) {
+    // we don't want to send REJECTED in mock mode here b/c it will reset the state incorrectly
+    dispatch({
+      type: `${FETCH_V2V_PLAN_REQUESTS}_FULFILLED`,
+      payload: requestPlanRequestsData.response
+    });
+  } else {
+    dispatch({
+      type: FETCH_V2V_PLAN_REQUESTS,
+      payload: API.get(url)
+    });
+  }
+};
 
 export const fetchPlanRequestsAction = url => {
   const uri = new URI(url);
