854b13c7cb13e71c8ceb3e64bad174de39842257
Mike Ro <mikerodev@gmail.com>
2018-04-16 11:22:06 -0400

    Store migrations filter state in redux
    * Preserves the selected filter so that the user returns to the same
    screen when navigating away from and back to the overview
    * Converts Migrations to a functional component

--- app/javascript/react/screens/App/Overview/components/Migrations/Migrations.js
+++ app/javascript/react/screens/App/Overview/components/Migrations/Migrations.js
@@ -6,122 +6,105 @@ import MigrationsNotStartedList from './MigrationsNotStartedList';
 import MigrationsCompletedList from './MigrationsCompletedList';
 import OverviewEmptyState from '../OverviewEmptyState/OverviewEmptyState';
 
-class Migrations extends React.Component {
-  static getDerivedStateFromProps(nextProps) {
-    const { activeFilter } = nextProps;
+const Migrations = ({
+  activeFilter,
+  setActiveFilter,
+  transformationPlans,
+  notStartedPlans,
+  activeTransformationPlans,
+  createMigrationPlanClick,
+  createTransformationPlanRequestClick,
+  isCreatingTransformationPlanRequest,
+  finishedTransformationPlans
+}) => {
+  const filterOptions = [
+    'Migration Plans Not Started',
+    'Migration Plans in Progress',
+    'Migration Plans Completed'
+  ];
 
-    if (nextProps.activeFilter === 'Migration Plans in Progress') {
-      return {
-        activeFilter
-      };
-    }
-    return null;
-  }
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeFilter: 'Migration Plans Not Started'
-    };
-  }
-  onSelect = eventKey => {
-    this.setState({ activeFilter: eventKey });
-  };
-
-  render() {
-    const {
-      transformationPlans,
-      notStartedPlans,
-      activeTransformationPlans,
-      createMigrationPlanClick,
-      createTransformationPlanRequestClick,
-      isCreatingTransformationPlanRequest,
-      finishedTransformationPlans
-    } = this.props;
-    const { activeFilter } = this.state;
-    const filterOptions = [
-      'Migration Plans Not Started',
-      'Migration Plans in Progress',
-      'Migration Plans Completed'
-    ];
-
-    return (
-      <React.Fragment>
-        <Grid.Col xs={12}>
-          <div className="heading-with-link-container">
-            <div className="pull-left">
-              <h3>{__('Migrations')}</h3>
-            </div>
-            <div className="pull-right">
-              {/** todo: create IconLink in patternfly-react * */}
-              <a
-                href="#"
-                onClick={e => {
-                  e.preventDefault();
-                  createMigrationPlanClick();
-                }}
-              >
-                <Icon type="pf" name="add-circle-o" />
-                &nbsp;{__('Create Migration Plan')}
-              </a>
-            </div>
+  return (
+    <React.Fragment>
+      <Grid.Col xs={12}>
+        <div className="heading-with-link-container">
+          <div className="pull-left">
+            <h3>{__('Migrations')}</h3>
           </div>
-          <hr style={{ borderTopColor: '#d1d1d1' }} />
-          {transformationPlans.length > 0 ? (
-            <div style={{ marginBottom: 15 }}>
-              <DropdownButton
-                bsStyle="default"
-                title={sprintf('%s', activeFilter)}
-                id="dropdown-filter"
-                onSelect={this.onSelect}
-              >
-                {filterOptions.map((filter, i) => (
-                  <MenuItem
-                    eventKey={filter}
-                    active={filter === activeFilter}
-                    key={i}
-                  >
-                    {sprintf('%s', filter)}
-                  </MenuItem>
-                ))}
-              </DropdownButton>
-            </div>
-          ) : (
-            <OverviewEmptyState
-              showWizardAction={createMigrationPlanClick}
-              description="Create a migration plan to select VMs for migration."
-              buttonText="Create Migration Plan"
+          <div className="pull-right">
+            {/** todo: create IconLink in patternfly-react * */}
+            <a
+              href="#"
+              onClick={e => {
+                e.preventDefault();
+                createMigrationPlanClick();
+              }}
+            >
+              <Icon type="pf" name="add-circle-o" />
+              &nbsp;{__('Create Migration Plan')}
+            </a>
+          </div>
+        </div>
+        <hr style={{ borderTopColor: '#d1d1d1' }} />
+        {transformationPlans.length > 0 ? (
+          <div style={{ marginBottom: 15 }}>
+            <DropdownButton
+              bsStyle="default"
+              title={sprintf('%s', activeFilter)}
+              id="dropdown-filter"
+              onSelect={eventKey => {
+                setActiveFilter(eventKey);
+              }}
+            >
+              {filterOptions.map((filter, i) => (
+                <MenuItem
+                  eventKey={filter}
+                  active={filter === activeFilter}
+                  key={i}
+                >
+                  {sprintf('%s', filter)}
+                </MenuItem>
+              ))}
+            </DropdownButton>
+          </div>
+        ) : (
+          <OverviewEmptyState
+            showWizardAction={createMigrationPlanClick}
+            description="Create a migration plan to select VMs for migration."
+            buttonText="Create Migration Plan"
+          />
+        )}
+      </Grid.Col>
+      {transformationPlans.length > 0 && (
+        <React.Fragment>
+          {activeFilter === 'Migration Plans Not Started' && (
+            <MigrationsNotStartedList
+              notStartedPlans={notStartedPlans}
+              migrateClick={createTransformationPlanRequestClick}
+              loading={isCreatingTransformationPlanRequest}
             />
           )}
-        </Grid.Col>
-        {transformationPlans.length > 0 && (
-          <React.Fragment>
-            {activeFilter === 'Migration Plans Not Started' && (
-              <MigrationsNotStartedList
-                notStartedPlans={notStartedPlans}
-                migrateClick={createTransformationPlanRequestClick}
-                loading={isCreatingTransformationPlanRequest}
-              />
-            )}
-            {activeFilter === 'Migration Plans in Progress' && (
-              <MigrationsInProgressCards
-                activeTransformationPlans={activeTransformationPlans}
-                loading={isCreatingTransformationPlanRequest !== null}
-              />
-            )}
-            {activeFilter === 'Migration Plans Completed' && (
-              <MigrationsCompletedList
-                finishedTransformationPlans={finishedTransformationPlans}
-                retryClick={createTransformationPlanRequestClick}
-                loading={isCreatingTransformationPlanRequest}
-              />
-            )}
-          </React.Fragment>
-        )}
-      </React.Fragment>
-    );
-  }
-}
+          {activeFilter === 'Migration Plans in Progress' && (
+            <MigrationsInProgressCards
+              activeTransformationPlans={activeTransformationPlans}
+              loading={isCreatingTransformationPlanRequest !== null}
+            />
+          )}
+          {activeFilter === 'Migration Plans Completed' && (
+            <MigrationsCompletedList
+              finishedTransformationPlans={finishedTransformationPlans}
+              retryClick={createTransformationPlanRequestClick}
+              loading={isCreatingTransformationPlanRequest}
+            />
+          )}
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
+};
+
 Migrations.propTypes = {
+  activeFilter: PropTypes.string,
+  setActiveFilter: PropTypes.func,
   transformationPlans: PropTypes.array,
   notStartedPlans: PropTypes.array,
   activeTransformationPlans: PropTypes.array,
