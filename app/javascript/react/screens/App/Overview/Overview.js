101bdd580f2ba519726eeb855c9fee482c6bc557
Patrick Riley <priley86@gmail.com>
2018-04-19 10:45:12 -0400

    fix aggregate card alignment

--- app/javascript/react/screens/App/Overview/Overview.js
+++ app/javascript/react/screens/App/Overview/Overview.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { bindMethods, Breadcrumb, CardGrid, Spinner } from 'patternfly-react';
+import {
+  bindMethods,
+  Card,
+  Breadcrumb,
+  CardGrid,
+  Spinner
+} from 'patternfly-react';
 import Toolbar from '../../../config/Toolbar';
 import * as AggregateCards from './components/AggregateCards';
 import InfrastructureMappingsList from './components/InfrastructureMappingsList/InfrastructureMappingsList';
@@ -145,14 +151,8 @@ class Overview extends React.Component {
     } = this.props;
 
     const aggregateDataCards = (
-      <CardGrid
-        matchHeight
-        style={{
-          marginLeft: 10,
-          marginRight: 10
-        }}
-      >
-        <CardGrid.Row>
+      <div className="row-cards-pf">
+        <Card.HeightMatching selector={['.card-pf-match-height']}>
           <CardGrid.Col xs={6} sm={3}>
             <AggregateCards.NotStartedTransformationPlans
               notStartedPlans={notStartedTransformationPlans}
@@ -177,8 +177,8 @@ class Overview extends React.Component {
               loading={isFetchingTransformationMappings}
             />
           </CardGrid.Col>
-        </CardGrid.Row>
-      </CardGrid>
+        </Card.HeightMatching>
+      </div>
     );
 
     const overviewContent = (
