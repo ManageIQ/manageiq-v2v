fa5513ed4a22598160edd289f110c1b21dcfa24b
Aparna Karve <akarve@redhat.com>
2018-04-30 13:52:04 -0700

    apply empty state commit

--- app/javascript/react/screens/App/Overview/components/Migrations/MigrationsInProgressCards.js
+++ app/javascript/react/screens/App/Overview/components/Migrations/MigrationsInProgressCards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Grid, EmptyState, Spinner } from 'patternfly-react';
+import OverviewEmptyState from '../OverviewEmptyState/OverviewEmptyState';
 import MigrationInProgressCard from './MigrationsInProgressCard';
 
 const MigrationsInProgressCards = ({
@@ -15,7 +16,7 @@ const MigrationsInProgressCards = ({
         <Card.HeightMatching selector={['.card-pf-match-height']}>
           <Spinner loading={loading}>
             {activeTransformationPlans.length > 0 &&
-              allRequestsWithTasks.length > 0 &&
+            allRequestsWithTasks.length > 0 ? (
               activeTransformationPlans.map(plan => (
                 <MigrationInProgressCard
                   plan={plan}
@@ -23,17 +24,24 @@ const MigrationsInProgressCards = ({
                   key={plan.id}
                   handleClick={redirectTo}
                 />
-              ))}
-            {activeTransformationPlans.length === 0 && (
-              <EmptyState>
-                <EmptyState.Icon />
-                <EmptyState.Title>
-                  {__('No Active Migrations')}
-                </EmptyState.Title>
-                <EmptyState.Info>
-                  {__('There are currently no active migrations.')}
-                </EmptyState.Info>
-              </EmptyState>
+              ))
+            ) : (
+              <OverviewEmptyState
+                title={__('No Migration Plans In Progress')}
+                iconType="pf"
+                iconName="info"
+                description={
+                  <div>
+                    {__(
+                      'There are no existing migration plans in an In Progress state.'
+                    )}
+                    <br />{' '}
+                    {__(
+                      'Make a selection in the dropdown to view plans in other states.'
+                    )}
+                  </div>
+                }
+              />
             )}
           </Spinner>
         </Card.HeightMatching>
