4c2005d68db6dfcd56f7d2fb50cc308d7b152569
Mike Turley <mturley@redhat.com>
2018-05-01 09:24:41 -0400

    Fix sprintf calls

--- app/javascript/common/constants.js
+++ app/javascript/common/constants.js
@@ -1,20 +1,28 @@
 export const MAX_LENGTH_NAMES = 24;
 export const MAX_LENGTH_DESCRIPTIONS = 128;
 
-const maxLengthIs = `Maximum length is %s characters.`;
-const requiredMaxLengthIs = `Required. ${maxLengthIs}`;
-const youHaveReachedMax = `You have reached the maximum length of %s characters.`;
-
 export const validation = {
   name: {
-    help: sprintf(__(maxLengthIs), MAX_LENGTH_NAMES),
-    requiredMessage: sprintf(__(requiredMaxLengthIs), MAX_LENGTH_NAMES),
+    help: sprintf(__(`Maximum length is %s characters.`), MAX_LENGTH_NAMES),
+    requiredMessage: sprintf(
+      __(`Required. Maximum length is %s characters.`),
+      MAX_LENGTH_NAMES
+    ),
     maxLength: MAX_LENGTH_NAMES,
-    maxLengthWarning: sprintf(__(youHaveReachedMax), MAX_LENGTH_NAMES)
+    maxLengthWarning: sprintf(
+      __(`You have reached the maximum length of %s characters.`),
+      MAX_LENGTH_NAMES
+    )
   },
   description: {
-    help: sprintf(__(maxLengthIs), MAX_LENGTH_DESCRIPTIONS),
+    help: sprintf(
+      __(`Maximum length is %s characters.`),
+      MAX_LENGTH_DESCRIPTIONS
+    ),
     maxLength: MAX_LENGTH_DESCRIPTIONS,
-    maxLengthWarning: sprintf(__(youHaveReachedMax), MAX_LENGTH_DESCRIPTIONS)
+    maxLengthWarning: sprintf(
+      __(`You have reached the maximum length of %s characters.`),
+      MAX_LENGTH_DESCRIPTIONS
+    )
   }
 };
