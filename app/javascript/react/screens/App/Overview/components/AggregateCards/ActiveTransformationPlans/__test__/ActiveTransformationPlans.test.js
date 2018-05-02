125e44600979c38b75ba03ca70068afe2f212e8c
Mike Ro <mikerodev@gmail.com>
2018-05-02 07:49:39 -0400

    Update transformation plan fixtures
    * Adjust fixtures for API adjustments made in
      https://github.com/priley86/miq_v2v_ui_plugin/pull/260
    
      This aligns mockMode responses with actual back end
    * Fix associated test
    * Fix Jest propType warning

--- app/javascript/react/screens/App/Overview/components/AggregateCards/ActiveTransformationPlans/__test__/ActiveTransformationPlans.test.js
+++ app/javascript/react/screens/App/Overview/components/AggregateCards/ActiveTransformationPlans/__test__/ActiveTransformationPlans.test.js
@@ -4,14 +4,18 @@ import { shallow } from 'enzyme';
 import ActiveTransformationPlans from '../ActiveTransformationPlans';
 import { activeTransformationPlansFilter } from '../../../../OverviewSelectors';
 import { transformationPlans } from '../../../../overview.transformationPlans.fixtures';
+import { allRequestsWithTasks } from '../../../../overview.requestWithTasks.fixtures';
 
 const { resources: plans } = transformationPlans;
+const { results: requests } = allRequestsWithTasks;
 const activePlans = activeTransformationPlansFilter(plans);
 
 test('displays the number of active transformation plans with an error', () => {
-  const [, activePlan, erroredPlan] = activePlans;
   const wrapper = shallow(
-    <ActiveTransformationPlans activePlans={[activePlan, erroredPlan]} />
+    <ActiveTransformationPlans
+      activePlans={activePlans}
+      allRequestsWithTasks={requests}
+    />
   );
 
   expect(wrapper.find('Icon').prop('name')).toBe('error-circle-o');
@@ -20,5 +24,5 @@ test('displays the number of active transformation plans with an error', () => {
       .find('AggregateStatusNotification')
       .childAt(2)
       .text()
-  ).toBe('2');
+  ).toBe('1');
 });
