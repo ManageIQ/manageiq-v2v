be109fef969d9e493f211e994a9bfffa56769bd3
Martin Hradil <mhradil@redhat.com>
2018-05-20 14:50:18 +0000

    rails controllers - use default set of action filters, including check_privileges

--- app/controllers/migration_controller.rb
+++ app/controllers/migration_controller.rb
@@ -1,6 +1,9 @@
 # Migration controller - mostly service to initialize the react top level component
 # and to handle the initial routing endpoint.
 class MigrationController < ApplicationController
+  before_action :check_privileges
+  after_action :cleanup_action
+
   def index
     # this sets the active menu item, must match the item name in lib/miq_v2v_ui/engine.rb
     @layout = 'migration'
