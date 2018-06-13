5280e50771585dee8e2cc1ba21b51241c8e9719e
Patrick Riley <priley86@gmail.com>
2018-06-13 15:10:32 -0400

    i18n support for infra mapping labels

--- app/javascript/common/helpers.js
+++ app/javascript/common/helpers.js
@@ -1,6 +1,3 @@
 // todo: expose this from patternfly-react instead?
 export const getDisplayName = Component =>
   Component.displayName || Component.name || 'Component';
-
-export const simplePluralize = (count, singular, plural) =>
-  count === 1 ? singular : plural;
