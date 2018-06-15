12ebc1211a581a501e59664bbee062d3d71852a4
Mike Turley <mturley@redhat.com>
2018-06-15 19:27:12 -0400

    Add endTime for failed tasks, and extend to tasks list

--- app/javascript/components/dates/TickingIsoElapsedTime.js
+++ app/javascript/components/dates/TickingIsoElapsedTime.js
@@ -3,15 +3,14 @@ import PropTypes from 'prop-types';
 import { IsoElapsedTime } from './IsoElapsedTime';
 
 class TickingIsoElapsedTime extends React.Component {
-  constructor() {
-    super();
-    this.state = { now: Date.now() };
+  constructor(props) {
+    super(props);
+    this.state = {
+      now: props.endTime || Date.now()
+    };
     this._tickInterval = null;
+    this.dateProps = this.dateProps.bind(this);
     this.tick = this.tick.bind(this);
-  }
-
-  tick() {
-    this.setState({ now: Date.now() });
   }
 
   componentDidMount() {
@@ -21,21 +20,42 @@ class TickingIsoElapsedTime extends React.Component {
   componentWillUnmount() {
     clearInterval(this._tickInterval);
   }
-  
-  render() {
-    const { startTime } = this.props;
-    const { now } = this.state;
-    return (
-      <React.Fragment>
-        {IsoElapsedTime(new Date(startTime), now)}
-      </React.Fragment>
-    );
+
+  dateProps() {
+    // The new Date() constructor, when passed a Date object, just returns that object.
+    // So it can be used to "cast" unknown string-or-Date props into known Date objects.
+    const { startTime, endTime } = this.props;
+    return {
+      startTime: new Date(startTime),
+      endTime: endTime ? new Date(endTime) : null
+    };
   }
 
+  tick() {
+    const { endTime } = this.dateProps();
+    this.setState({
+      now: endTime || Date.now()
+    });
+  }
+
+  render() {
+    const { startTime } = this.dateProps();
+    const { now } = this.state;
+    return <React.Fragment>{IsoElapsedTime(startTime, now)}</React.Fragment>;
+  }
 }
 
 TickingIsoElapsedTime.propTypes = {
-  startTime: PropTypes.string, // any string you can pass to new Date()
+  startTime: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.instanceOf(Date)
+  ]),
+  endTime: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date)])
+};
+
+TickingIsoElapsedTime.defaultProps = {
+  endTime: null // If a null endTime is passed, the time will tick each second.
+  // A non-null endTime will stop the ticking and just display the difference.
 };
 
 export default TickingIsoElapsedTime;
