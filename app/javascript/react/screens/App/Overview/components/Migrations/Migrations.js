a2ef240f1edebadf86111efd74f1eca56f195649
Aparna Karve <akarve@redhat.com>
2018-06-29 16:18:27 -0700

    request and task math to get previous task records

--- app/javascript/react/screens/App/Overview/components/Migrations/Migrations.js
+++ app/javascript/react/screens/App/Overview/components/Migrations/Migrations.js
@@ -117,6 +117,7 @@ const Migrations = ({
             <MigrationsCompletedList
               finishedTransformationPlans={finishedTransformationPlans}
               allRequestsWithTasks={allRequestsWithTasks}
+              allTasks={allRequestsWithTasks.map(request => request.miq_request_tasks)}
               retryClick={createTransformationPlanRequestClick}
               loading={isCreatingTransformationPlanRequest}
               redirectTo={redirectTo}
