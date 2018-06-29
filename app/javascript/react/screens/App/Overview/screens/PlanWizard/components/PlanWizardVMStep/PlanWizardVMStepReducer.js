584a9cb769852b320257eb5450868afbb0c291b1
Allen Wight <allen.b.wight@gmail.com>
2018-06-29 16:08:49 -0400

    Merge pull request #450 from AparnaKarve/make_conflict_vms_selectable
    VMs in conflict could be valid hence make them selectable with a warning
    (cherry picked from commit 9d22393218ab7056a6c2d3e3787f0e21c7cf2b1a)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1596850

--- app/javascript/react/screens/App/Overview/screens/PlanWizard/components/PlanWizardVMStep/PlanWizardVMStepReducer.js
+++ app/javascript/react/screens/App/Overview/screens/PlanWizard/components/PlanWizardVMStep/PlanWizardVMStepReducer.js
@@ -76,14 +76,32 @@ const _formatInvalidVms = vms => {
   );
 };
 
-const _formatConflictVms = vms =>
-  vms &&
-  vms.map(v => {
-    v.conflict = true;
-    v.allocated_size = numeral(v.allocated_size).format('0.00b');
-    v.reason = V2V_VM_POST_VALIDATION_REASONS[v.reason];
-    return v;
-  });
+const _formatConflictVms = vms => {
+  const inactiveVM = vms && vms.filter(vm => vm.cluster === '' || vm.path === '');
+  const inactiveVMCount = inactiveVM.length;
+  const vmCount = inactiveVMCount > 0 ? vms.length - inactiveVMCount : vms.length;
+  const uniqueIds = vms && [...new Set(vms.map(value => value.id))];
+  return (
+    vms &&
+    vms.map((v, vIndex) => {
+      v.allocated_size = numeral(v.allocated_size).format('0.00b');
+      v.reason = V2V_VM_POST_VALIDATION_REASONS[v.reason];
+
+      if (v.cluster === '' || v.path === '') {
+        v.reason = V2V_VM_POST_VALIDATION_REASONS.inactive;
+        v.invalid = true;
+      } else if (vmCount === 1) {
+        v.reason = V2V_VM_POST_VALIDATION_REASONS.ok;
+        v.valid = true;
+      } else if (vmCount > 1) {
+        v.warning = true;
+      }
+
+      manageOddCSVImportErrors(v, vIndex, uniqueIds);
+      return v;
+    })
+  );
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
