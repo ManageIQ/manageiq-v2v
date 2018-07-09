add8150505a261409657241093226c118fd0dae5
Aparna Karve <akarve@redhat.com>
2018-07-09 15:34:29 -0700

    Migration status string constants for i18n

--- app/javascript/react/screens/App/Plan/PlanConstants.js
+++ app/javascript/react/screens/App/Plan/PlanConstants.js
@@ -6,3 +6,25 @@ export const RESET_PLAN_STATE = 'RESET_PLAN_STATE';
 export const FETCH_V2V_MIGRATION_TASK_LOG = 'FETCH_V2V_MIGRATION_TASK_LOG';
 export const DOWNLOAD_LOG_CLICKED = 'DOWNLOAD_LOG_CLICKED';
 export const DOWNLOAD_LOG_COMPLETED = 'DOWNLOAD_LOG_COMPLETED';
+
+export const V2V_MIGRATION_STATUS_MESSAGES = {
+  'Acquire Transformation Host': __('Acquire Transformation Host'),
+  'Assess Migration': __('Assess Migration'),
+  'Automation Starting': __('Automation Starting'),
+  'Collapse Snapshots': __('Collapse Snapshots'),
+  'Convert disks': __('Convert disks'),
+  'Mark source as migrated': __('Mark source as migrated'),
+  Migrating: __('Migrating'),
+  'Migration complete': __('Migration complete'),
+  'Power off': __('Power off'),
+  'Power-on VM': __('Power-on VM'),
+  'Pre-migration': __('Pre-migration'),
+  'Refresh inventory': __('Refresh inventory'),
+  'Restore VM Attributes': __('Restore VM Attributes'),
+  'Transform VM': __('Transform VM'),
+  'Update description of VM': __('Update description of VM'),
+  Validating: __('Validating'),
+  'VM Transformations completed': __('VM Transformations completed'),
+  'Virtual machine migrated': __('Virtual machine migrated'),
+  'VM Transformations failed': __('VM Transformations failed')
+};
