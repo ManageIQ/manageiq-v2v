f1b37206cd66d6b1a1efccd884fcdda97c3fd718
Patrick Riley <priley86@gmail.com>
2018-07-16 17:09:05 -0400

    add migration overview pre/post playbook in progress UI

---
+++ app/javascript/react/screens/App/Overview/components/Migrations/InProgressCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Grid, noop } from 'patternfly-react';
+
+const InProgressCard = ({ title, children, onClick, ...props }) => (
+  <Grid.Col sm={12} md={6} lg={4}>
+    <Card matchHeight onClick={onClick} {...props}>
+      <Card.Heading>{title}</Card.Heading>
+      <Card.Body>{children}</Card.Body>
+    </Card>
+  </Grid.Col>
+);
+
+InProgressCard.propTypes = {
+  title: PropTypes.node,
+  children: PropTypes.node,
+  onClick: PropTypes.func
+};
+
+InProgressCard.defaultProps = {
+  title: '',
+  children: null,
+  onClick: noop
+};
+
+export default InProgressCard;
