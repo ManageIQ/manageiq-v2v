a98bd9dbba1ea2322922086255a01bfe93b07046
Martin Maroši <marvusm.mmi@gmail.com>
2018-08-03 10:27:39 +0200

    Use manageiq mounting for react component

--- app/javascript/packs/migration.js
+++ app/javascript/packs/migration.js
@@ -1,14 +1,13 @@
 import './polyfills';
 import componentRegistry from '../components/componentRegistry';
-import { coreComponents } from '../components';
 import { mount } from '../components/mounter';
-
-componentRegistry.registerMultiple(coreComponents);
+import App from '../react/';
 
 window.v2v = {
   mount,
   componentRegistry
 };
 
+ManageIQ.component.addReact('manageiq-v2v', App);
 // Another way to mount the component is via JS - e.g.
 // v2v.mount('manageiq-v2v', '#reactRoot');
