3f6343d69afd5ddabecf7ee1f6f3f9fd21966a2b
Aparna Karve <akarve@redhat.com>
2018-08-20 12:14:38 -0700

    one more round of verification

--- app/controllers/migration_log_controller.rb
+++ app/controllers/migration_log_controller.rb
@@ -9,18 +9,18 @@ class MigrationLogController < ApplicationController
   private
 
   def migration_log_json(transformation_plan_task_id)
-    plan_task = ServiceTemplateTransformationPlanTask.find(transformation_plan_task_id)
-    miq_tasks_id = plan_task.transformation_log_queue()
-    task = MiqTask.wait_for_taskid(miq_tasks_id)
-    task_results = task.task_results
-    task_status = task.status
-    task_message = task.message
-    MiqTask.destroy(task)
-
-    {
-      :log_contents   => task_results,
-      :status         => task_status,
-      :status_message => task_message
-    }
+    # plan_task = ServiceTemplateTransformationPlanTask.find(transformation_plan_task_id)
+    # miq_tasks_id = plan_task.transformation_log_queue()
+    # task = MiqTask.wait_for_taskid(miq_tasks_id)
+    # task_results = task.task_results
+    # task_status = task.status
+    # task_message = task.message
+    # MiqTask.destroy(task)
+    #
+    # {
+    #   :log_contents   => task_results,
+    #   :status         => task_status,
+    #   :status_message => task_message
+    # }
   end
 end
