e31706f9f081a327f2c8162d65cdaa6c251d6c23
Aparna Karve <akarve@redhat.com>
2018-08-20 11:37:58 -0700

    use a private method to handle the log rendering

--- app/controllers/migration_log_controller.rb
+++ app/controllers/migration_log_controller.rb
@@ -3,7 +3,13 @@ class MigrationLogController < ApplicationController
   after_action :cleanup_action
 
   def download_migration_log
-    plan_task = ServiceTemplateTransformationPlanTask.find(params[:id])
+    render :json => migration_log_json(params[:id])
+  end
+
+  private
+
+  def migration_log_json(transformation_plan_task_id)
+    plan_task = ServiceTemplateTransformationPlanTask.find(transformation_plan_task_id)
     miq_tasks_id = plan_task.transformation_log_queue()
     task = MiqTask.wait_for_taskid(miq_tasks_id)
     task_results = task.task_results
@@ -11,7 +17,7 @@ class MigrationLogController < ApplicationController
     task_message = task.message
     MiqTask.destroy(task)
 
-    render :json => {
+    {
       :log_contents   => task_results,
       :status         => task_status,
       :status_message => task_message
