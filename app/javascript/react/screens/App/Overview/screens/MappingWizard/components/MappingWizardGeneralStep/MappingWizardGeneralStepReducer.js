70930b113e55e792e0179e26b178fd6a27bacda3
Mike Ro <mikerodev@gmail.com>
2018-09-19 09:08:20 -0400

    Clear editingPlan if wizard is exited

--- app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardGeneralStep/MappingWizardGeneralStepReducer.js
+++ app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardGeneralStep/MappingWizardGeneralStepReducer.js
@@ -1,15 +1,19 @@
 import Immutable from 'seamless-immutable';
 
 import { SET_V2V_EDITING_MAPPING } from './MappingWizardGeneralStepConstants';
+import { MAPPING_WIZARD_EXITED } from '../../../../OverviewConstants';
 
-const initialState = Immutable({
-  editingMapping: {}
+export const initialState = Immutable({
+  editingMapping: null
 });
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case SET_V2V_EDITING_MAPPING:
       return state.set('editingMapping', action.payload);
+
+    case MAPPING_WIZARD_EXITED:
+      return state.set('editingMapping', null);
 
     default:
       return state;
