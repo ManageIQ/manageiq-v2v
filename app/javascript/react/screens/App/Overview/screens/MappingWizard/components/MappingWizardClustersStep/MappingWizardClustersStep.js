0c6f7e054a2c6664876b949ec170149dd5967090
Mike Ro <mikerodev@gmail.com>
2018-09-22 14:46:31 -0400

    Do not fetch hosts for OSP tenants

--- app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardClustersStep/MappingWizardClustersStep.js
+++ app/javascript/react/screens/App/Overview/screens/MappingWizard/components/MappingWizardClustersStep/MappingWizardClustersStep.js
@@ -24,7 +24,12 @@ class MappingWizardClustersStep extends React.Component {
 
     fetchSourceClustersAction(fetchSourceClustersUrl);
     fetchTargetClustersAction(fetchTargetComputeUrls[targetProvider]).then(result => {
-      if (result.value && result.value.data && result.value.data.resources.length > 0) {
+      if (
+        targetProvider !== 'openstack' &&
+        result.value &&
+        result.value.data &&
+        result.value.data.resources.length > 0
+      ) {
         const hostIDsByClusterID = result.value.data.resources.reduce(
           (newObject, cluster) => ({
             ...newObject,
