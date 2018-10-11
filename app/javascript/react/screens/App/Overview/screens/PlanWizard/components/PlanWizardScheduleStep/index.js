bce351232e58be89c4c7790b31212cc604604c22
Mike Turley <mturley@redhat.com>
2018-10-11 16:49:21 -0400

    [#687] Add warning for openstack VMs to schedule step

--- app/javascript/react/screens/App/Overview/screens/PlanWizard/components/PlanWizardScheduleStep/index.js
+++ app/javascript/react/screens/App/Overview/screens/PlanWizard/components/PlanWizardScheduleStep/index.js
@@ -1,10 +1,19 @@
 import { connect } from 'react-redux';
 import PlanWizardScheduleStep from './PlanWizardScheduleStep';
+import { getMappingType } from '../../../../components/InfrastructureMappingsList/helpers';
 
-const mapStateToProps = () => ({
-  initialValues: {
-    migration_plan_choice_radio: 'migration_plan_later'
-  }
-});
+const mapStateToProps = ({ overview, form }) => {
+  const mappingId = form.planWizardGeneralStep.values.infrastructure_mapping;
+  const selectedMapping = overview.transformationMappings.find(mapping => mapping.id === mappingId);
+  const targetProvider = getMappingType(selectedMapping.transformation_mapping_items);
+  return {
+    targetProvider,
+    migration_plan_choice_radio:
+      form.planWizardScheduleStep && form.planWizardScheduleStep.values.migration_plan_choice_radio,
+    initialValues: {
+      migration_plan_choice_radio: 'migration_plan_later'
+    }
+  };
+};
 
 export default connect(mapStateToProps)(PlanWizardScheduleStep);
