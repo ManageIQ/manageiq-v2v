9371bfa129c7a885288d93cea9504972700ee55c
Martin Hradil <mhradil@redhat.com>
2018-11-01 15:49:30 +0000

    add redirect from old migration url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # ManageIQ::V2V::Engine.routes.draw do
 Rails.application.routes.draw do
+  match '/migration' => redirect('/migration/overview'), :via => [:get]
   match '/migration/overview' => 'migration#index', :via => [:get]
   match '/migration/mappings' => 'migration#index', :via => [:get]
   match '/migration/plan/:id' => 'migration#index', :via => [:get]
