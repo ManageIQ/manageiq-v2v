d20d53743cbec6a04b1e42b9f692ab46c001ce84
Martin Maroši <marvusm.mmi@gmail.com>
2018-11-08 13:46:01 +0100

    Remove outdated mount_react_component.

--- app/javascript/packs/migration.js
+++ app/javascript/packs/migration.js
@@ -1,16 +1,8 @@
 import './polyfills';
 import componentRegistry from '../components/componentRegistry';
-import { mount } from '../components/mounter';
 import App from '../react/';
 import { coreComponents } from '../components';
 
 componentRegistry.registerMultiple(coreComponents);
 
-window.v2v = {
-  mount,
-  componentRegistry
-};
-
 ManageIQ.component.addReact('manageiq-v2v', App);
-// Another way to mount the component is via JS - e.g.
-// v2v.mount('manageiq-v2v', '#reactRoot');
