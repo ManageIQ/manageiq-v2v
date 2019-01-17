f5043e39aef2071f3a17c8b0a64144fb741cafec
Mike Turley <mturley@redhat.com>
2019-01-17 15:17:15 -0500

    Fix existing tests

--- app/javascript/react/screens/App/Settings/__tests__/GeneralSettings.test.js
+++ app/javascript/react/screens/App/Settings/__tests__/GeneralSettings.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Spinner } from 'patternfly-react';
 import { shallow } from 'enzyme';
-import { Settings } from '../Settings';
+import { GeneralSettings } from '../GeneralSettings';
 import { servers, settings } from '../settings.fixures';
 import { getFormValuesFromApiSettings } from '../helpers';
 
-describe('Settings component', () => {
+describe('GeneralSettings component', () => {
   const defaultFormValues = getFormValuesFromApiSettings(settings);
   const getBaseProps = () => ({
     fetchServersAction: jest.fn(),
@@ -16,20 +16,20 @@ describe('Settings component', () => {
     servers: servers.resources
   });
 
-  it('renders the settings page', () => {
-    const component = shallow(<Settings {...getBaseProps()} />);
+  it('renders the general settings page', () => {
+    const component = shallow(<GeneralSettings {...getBaseProps()} />);
     expect(component).toMatchSnapshot();
   });
 
-  it('renders the settings page with Applying spinner when saving', () => {
-    const component = shallow(<Settings {...getBaseProps()} isSavingSettings />);
+  it('renders the general settings page with Applying spinner when saving', () => {
+    const component = shallow(<GeneralSettings {...getBaseProps()} isSavingSettings />);
     const applying = component.find(Spinner).findWhere(child => child.text() === ' Applying...');
     expect(applying).toHaveLength(1);
   });
 
   it('properly calls patchSettingsAction on apply click', () => {
     const patchSettingsAction = jest.fn();
-    const component = shallow(<Settings {...getBaseProps()} patchSettingsAction={patchSettingsAction} />);
+    const component = shallow(<GeneralSettings {...getBaseProps()} patchSettingsAction={patchSettingsAction} />);
     component.find('Button').simulate('click');
     expect(patchSettingsAction).toBeCalledWith(servers.resources, defaultFormValues);
   });
