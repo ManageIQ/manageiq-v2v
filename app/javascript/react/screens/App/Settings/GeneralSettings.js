c488a744eb9ccb71ae3d0bf94cefe4caefd060a9
Mike Turley <mturley@redhat.com>
2019-01-17 14:19:46 -0500

    Break GeneralSettings out from Settings component

---
+++ app/javascript/react/screens/App/Settings/GeneralSettings.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { reduxForm, Field } from 'redux-form';
+import { Form, Button, Spinner } from 'patternfly-react';
+import NumberInput from '../common/forms/NumberInput';
+
+export class GeneralSettings extends React.Component {
+  componentDidMount() {
+    const { fetchServersAction, fetchServersUrl, fetchSettingsAction, fetchSettingsUrl } = this.props;
+    fetchServersAction(fetchServersUrl);
+    fetchSettingsAction(fetchSettingsUrl);
+  }
+
+  onApplyClick = () => {
+    const { patchSettingsAction, servers, settingsForm } = this.props;
+    patchSettingsAction(servers, settingsForm.values);
+  };
+
+  render() {
+    const { isFetchingServers, isFetchingSettings, isSavingSettings, savedSettings, settingsForm } = this.props;
+
+    const hasUnsavedChanges =
+      settingsForm &&
+      settingsForm.values &&
+      Object.keys(savedSettings).some(key => savedSettings[key] !== settingsForm.values[key]);
+
+    return (
+      <Spinner loading={isFetchingServers || isFetchingSettings} style={{ marginTop: 15 }}>
+        <div className="migration-settings">
+          <h2>{__('Concurrent Migrations')}</h2>
+          <Form style={{ padding: '0 20px' }}>
+            <Form.FormGroup>
+              <Form.ControlLabel>{__('Maximum concurrent migrations per conversion host')}</Form.ControlLabel>
+              <div style={{ width: 100 }}>
+                <Field
+                  id="max_concurrent_tasks_per_host"
+                  name="max_concurrent_tasks_per_host"
+                  component={NumberInput}
+                  normalize={NumberInput.normalizeStringToInt}
+                  min={1}
+                />
+              </div>
+            </Form.FormGroup>
+            <Form.FormGroup>
+              <Button bsStyle="primary" onClick={this.onApplyClick} disabled={!hasUnsavedChanges || isSavingSettings}>
+                {__('Apply')}
+              </Button>
+              <br />
+              {isSavingSettings && (
+                <div style={{ paddingTop: 10 }}>
+                  <Spinner loading size="xs" inline />
+                  {__(' Applying...')}
+                </div>
+              )}
+            </Form.FormGroup>
+          </Form>
+        </div>
+      </Spinner>
+    );
+  }
+}
+
+GeneralSettings.propTypes = {
+  fetchServersAction: PropTypes.func,
+  fetchSettingsAction: PropTypes.func,
+  patchSettingsAction: PropTypes.func,
+  isFetchingServers: PropTypes.bool,
+  isFetchingSettings: PropTypes.bool,
+  isSavingSettings: PropTypes.bool,
+  servers: PropTypes.array,
+  savedSettings: PropTypes.object,
+  settingsForm: PropTypes.object,
+  fetchServersUrl: PropTypes.string,
+  fetchSettingsUrl: PropTypes.string
+};
+
+GeneralSettings.defaultProps = {
+  fetchServersUrl: '/api/servers',
+  fetchSettingsUrl: '/api/settings'
+};
+
+export default reduxForm({
+  form: 'settings'
+})(GeneralSettings);
--- app/javascript/react/screens/App/Settings/Settings.js
+++ app/javascript/react/screens/App/Settings/Settings.js
@@ -1,100 +1,48 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { reduxForm, Field } from 'redux-form';
-import { Breadcrumb, Form, Button, Spinner } from 'patternfly-react';
+import { Breadcrumb } from 'patternfly-react';
 import Toolbar from '../../../config/Toolbar';
-import NumberInput from '../common/forms/NumberInput';
+import GeneralSettings from './GeneralSettings';
 
-export class Settings extends React.Component {
-  componentDidMount() {
-    const { fetchServersAction, fetchServersUrl, fetchSettingsAction, fetchSettingsUrl } = this.props;
-    fetchServersAction(fetchServersUrl);
-    fetchSettingsAction(fetchSettingsUrl);
-  }
+const Settings = props => {
+  const {
+    fetchServersAction,
+    fetchSettingsAction,
+    patchSettingsAction,
+    isFetchingServers,
+    isFetchingSettings,
+    isSavingSettings,
+    servers,
+    savedSettings,
+    settingsForm
+  } = props;
 
-  onApplyClick = () => {
-    const { patchSettingsAction, servers, settingsForm } = this.props;
-    patchSettingsAction(servers, settingsForm.values);
-  };
+  const toolbarContent = (
+    <Toolbar>
+      <Breadcrumb.Item href="/dashboard/maintab?tab=compute">{__('Compute')}</Breadcrumb.Item>
+      <Breadcrumb.Item href="#/plans">{__('Migration')}</Breadcrumb.Item>
+      <Breadcrumb.Item active>{__('Migration Settings')}</Breadcrumb.Item>
+    </Toolbar>
+  );
 
-  render() {
-    const { isFetchingServers, isFetchingSettings, isSavingSettings, savedSettings, settingsForm } = this.props;
-
-    const toolbarContent = (
-      <Toolbar>
-        <Breadcrumb.Item href="/dashboard/maintab?tab=compute">{__('Compute')}</Breadcrumb.Item>
-        <Breadcrumb.Item href="#/plans">{__('Migration')}</Breadcrumb.Item>
-        <Breadcrumb.Item active>{__('Migration Settings')}</Breadcrumb.Item>
-      </Toolbar>
-    );
-
-    const hasUnsavedChanges =
-      settingsForm &&
-      settingsForm.values &&
-      Object.keys(savedSettings).some(key => savedSettings[key] !== settingsForm.values[key]);
-
-    const settingsContent = (
-      <Spinner loading={isFetchingServers || isFetchingSettings} style={{ marginTop: 15 }}>
-        <div className="migration-settings">
-          <h2>{__('Concurrent Migrations')}</h2>
-          <Form style={{ padding: '0 20px' }}>
-            <Form.FormGroup>
-              <Form.ControlLabel>{__('Maximum concurrent migrations per conversion host')}</Form.ControlLabel>
-              <div style={{ width: 100 }}>
-                <Field
-                  id="max_concurrent_tasks_per_host"
-                  name="max_concurrent_tasks_per_host"
-                  component={NumberInput}
-                  normalize={NumberInput.normalizeStringToInt}
-                  min={1}
-                />
-              </div>
-            </Form.FormGroup>
-            <Form.FormGroup>
-              <Button bsStyle="primary" onClick={this.onApplyClick} disabled={!hasUnsavedChanges || isSavingSettings}>
-                {__('Apply')}
-              </Button>
-              <br />
-              {isSavingSettings && (
-                <div style={{ paddingTop: 10 }}>
-                  <Spinner loading size="xs" inline />
-                  {__(' Applying...')}
-                </div>
-              )}
-            </Form.FormGroup>
-          </Form>
-        </div>
-      </Spinner>
-    );
-
-    return (
-      <React.Fragment>
-        {toolbarContent}
-        {settingsContent}
-      </React.Fragment>
-    );
-  }
-}
-
-Settings.propTypes = {
-  fetchServersAction: PropTypes.func,
-  fetchSettingsAction: PropTypes.func,
-  patchSettingsAction: PropTypes.func,
-  isFetchingServers: PropTypes.bool,
-  isFetchingSettings: PropTypes.bool,
-  isSavingSettings: PropTypes.bool,
-  servers: PropTypes.array,
-  savedSettings: PropTypes.object,
-  settingsForm: PropTypes.object,
-  fetchServersUrl: PropTypes.string,
-  fetchSettingsUrl: PropTypes.string
+  return (
+    <React.Fragment>
+      {toolbarContent}
+      <GeneralSettings
+        fetchServersAction={fetchServersAction}
+        fetchSettingsAction={fetchSettingsAction}
+        patchSettingsAction={patchSettingsAction}
+        isFetchingServers={isFetchingServers}
+        isFetchingSettings={isFetchingSettings}
+        isSavingSettings={isSavingSettings}
+        servers={servers}
+        savedSettings={savedSettings}
+        settingsForm={settingsForm}
+        initialValues={savedSettings}
+        enableReinitialize
+        destroyOnUnmount={false}
+      />
+    </React.Fragment>
+  );
 };
 
-Settings.defaultProps = {
-  fetchServersUrl: '/api/servers',
-  fetchSettingsUrl: '/api/settings'
-};
-
-export default reduxForm({
-  form: 'settings'
-})(Settings);
+export default Settings;
