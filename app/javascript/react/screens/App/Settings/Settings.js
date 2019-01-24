75686ef4ca01d74f2c7af03c4c5bd2ef7d5d2b3b
Michael Ro <mikerodev@gmail.com>
2019-01-24 15:31:21 -0500

    Merge pull request #851 from mturley/settings-tabs
    Add routed tabs to the Settings page to prepare for Conversion Hosts settings
    
    (cherry picked from commit 339f3bf847c95c693f45af5abc107a0369f912ca)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1696423

--- app/javascript/react/screens/App/Settings/Settings.js
+++ app/javascript/react/screens/App/Settings/Settings.js
@@ -1,100 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { reduxForm, Field } from 'redux-form';
-import { Breadcrumb, Form, Button, Spinner } from 'patternfly-react';
+import { Breadcrumb, Tabs, Tab } from 'patternfly-react';
 import Toolbar from '../../../config/Toolbar';
-import NumberInput from '../common/forms/NumberInput';
+import GeneralSettings from './screens/GeneralSettings';
+import ConversionHostsSettings from './screens/ConversionHostsSettings';
 
-export class Settings extends React.Component {
-  componentDidMount() {
-    const { fetchServersAction, fetchServersUrl, fetchSettingsAction, fetchSettingsUrl } = this.props;
-    fetchServersAction(fetchServersUrl);
-    fetchSettingsAction(fetchSettingsUrl);
-  }
+const Settings = props => {
+  const { match, redirectTo } = props;
 
-  onApplyClick = () => {
-    const { patchSettingsAction, servers, settingsForm } = this.props;
-    patchSettingsAction(servers, settingsForm.values);
-  };
+  // TODO remove this when we are ready to release ConversionHostsSettings
+  const hideConversionHostSettings = true;
 
-  render() {
-    const { isFetchingServers, isFetchingSettings, isSavingSettings, savedSettings, settingsForm } = this.props;
-
-    const toolbarContent = (
+  return (
+    <React.Fragment>
       <Toolbar>
         <Breadcrumb.Item href="/dashboard/maintab?tab=compute">{__('Compute')}</Breadcrumb.Item>
         <Breadcrumb.Item href="#/plans">{__('Migration')}</Breadcrumb.Item>
         <Breadcrumb.Item active>{__('Migration Settings')}</Breadcrumb.Item>
       </Toolbar>
-    );
-
-    const hasUnsavedChanges =
-      settingsForm &&
-      settingsForm.values &&
-      Object.keys(savedSettings).some(key => savedSettings[key] !== settingsForm.values[key]);
-
-    const settingsContent = (
-      <Spinner loading={isFetchingServers || isFetchingSettings} style={{ marginTop: 15 }}>
-        <div className="migration-settings">
+      {hideConversionHostSettings ? (
+        <React.Fragment>
           <h2>{__('Concurrent Migrations')}</h2>
-          <Form style={{ padding: '0 20px' }}>
-            <Form.FormGroup>
-              <Form.ControlLabel>{__('Maximum concurrent migrations per conversion host')}</Form.ControlLabel>
-              <div style={{ width: 100 }}>
-                <Field
-                  id="max_concurrent_tasks_per_host"
-                  name="max_concurrent_tasks_per_host"
-                  component={NumberInput}
-                  normalize={NumberInput.normalizeStringToInt}
-                  min={1}
-                />
-              </div>
-            </Form.FormGroup>
-            <Form.FormGroup>
-              <Button bsStyle="primary" onClick={this.onApplyClick} disabled={!hasUnsavedChanges || isSavingSettings}>
-                {__('Apply')}
-              </Button>
-              <br />
-              {isSavingSettings && (
-                <div style={{ paddingTop: 10 }}>
-                  <Spinner loading size="xs" inline />
-                  {__(' Applying...')}
-                </div>
-              )}
-            </Form.FormGroup>
-          </Form>
+          <GeneralSettings />
+        </React.Fragment>
+      ) : (
+        <div style={{ marginTop: 10 }}>
+          <Tabs id="settings-tabs" activeKey={match.path} onSelect={key => redirectTo(key)}>
+            <Tab eventKey="/settings" title={__('Concurrent Migrations')}>
+              <GeneralSettings />
+            </Tab>
+            <Tab eventKey="/settings/conversion_hosts" title={__('Conversion Hosts')}>
+              <ConversionHostsSettings />
+            </Tab>
+          </Tabs>
         </div>
-      </Spinner>
-    );
-
-    return (
-      <React.Fragment>
-        {toolbarContent}
-        {settingsContent}
-      </React.Fragment>
-    );
-  }
-}
+      )}
+    </React.Fragment>
+  );
+};
 
 Settings.propTypes = {
-  fetchServersAction: PropTypes.func,
-  fetchSettingsAction: PropTypes.func,
-  patchSettingsAction: PropTypes.func,
-  isFetchingServers: PropTypes.bool,
-  isFetchingSettings: PropTypes.bool,
-  isSavingSettings: PropTypes.bool,
-  servers: PropTypes.array,
-  savedSettings: PropTypes.object,
-  settingsForm: PropTypes.object,
-  fetchServersUrl: PropTypes.string,
-  fetchSettingsUrl: PropTypes.string
+  match: PropTypes.object,
+  redirectTo: PropTypes.func
 };
 
-Settings.defaultProps = {
-  fetchServersUrl: '/api/servers',
-  fetchSettingsUrl: '/api/settings'
-};
-
-export default reduxForm({
-  form: 'settings'
-})(Settings);
+export default Settings;
