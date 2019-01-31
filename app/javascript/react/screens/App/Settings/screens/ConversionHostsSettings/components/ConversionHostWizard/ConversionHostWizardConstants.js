dd5b29004005bdd94164b596e4dd193a24651426
Mike Turley <mturley@redhat.com>
2019-01-31 17:28:02 -0500

    Add logic for Location step fields

--- app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostWizard/ConversionHostWizardConstants.js
+++ app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostWizard/ConversionHostWizardConstants.js
@@ -1,6 +1,13 @@
+import { RHV, OPENSTACK } from '../../../../../../../../common/constants';
+
 export const stepIDs = {
   locationStep: 'conversionHostWizardLocationStep',
   hostsStep: 'conversionHostWizardHostsStep',
   authenticationStep: 'conversionHostWizardAuthenticationStep',
   resultsStep: 'conversionHostWizardResultsStep'
+};
+
+export const PROVIDER_TYPES = {
+  [RHV]: 'ManageIQ::Providers::Redhat::InfraManager',
+  [OPENSTACK]: 'ManageIQ::Providers::Openstack::CloudManager'
 };
--- app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostWizard/ConversionHostWizardLocationStep/ConversionHostWizardLocationStep.js
+++ app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostWizard/ConversionHostWizardLocationStep/ConversionHostWizardLocationStep.js
@@ -4,29 +4,69 @@ import { reduxForm, Field, reset } from 'redux-form';
 import { required } from 'redux-form-validators';
 import { Form } from 'patternfly-react';
 
-import { FormField } from '../../../../../../common/forms/FormField';
 import { BootstrapSelect } from '../../../../../../common/forms/BootstrapSelect';
-import { V2V_TARGET_PROVIDERS } from '../../../../../../../../../common/constants';
+import { V2V_TARGET_PROVIDERS, RHV, OPENSTACK } from '../../../../../../../../../common/constants';
 
-import { stepIDs } from '../ConversionHostWizardConstants';
+import { stepIDs, PROVIDER_TYPES } from '../ConversionHostWizardConstants';
 
 export const ConversionHostWizardLocationStep = ({ locationStepForm }) => {
   const selectedProviderType = locationStepForm.values && locationStepForm.values.providerType;
+  const selectedProvider = locationStepForm.values && locationStepForm.values.provider;
+
+  const providers = []; // TODO /api/providers filtered by PROVIDER_TYPES?
+  const rhvClusters = []; // TODO
+  const ospProjects = []; // TODO
+
+  const selectFieldBaseProps = {
+    component: BootstrapSelect,
+    labelWidth: 2,
+    controlWidth: 9,
+    inline_label: true,
+    required: true
+  };
+
   return (
     <Form className="form-horizontal">
       <Field
+        {...selectFieldBaseProps}
         name="providerType"
         label={__('Provider Type')}
-        required
-        component={BootstrapSelect}
         options={V2V_TARGET_PROVIDERS}
         option_key="id"
         option_value="name"
-        labelWidth={2}
-        controlWidth={9}
-        inline_label
       />
-      Selected: {selectedProviderType || 'none'}
+      {selectedProviderType && (
+        <Field
+          {...selectFieldBaseProps}
+          name="provider"
+          label={__('Provider')}
+          options={providers}
+          option_key="id"
+          option_value="name"
+        />
+      )}
+      {selectedProviderType === RHV && (
+        <Field
+          {...selectFieldBaseProps}
+          name="cluster"
+          label={__('Cluster')}
+          options={rhvClusters}
+          option_key="id"
+          option_value="name"
+          disabled={!selectedProvider}
+        />
+      )}
+      {selectedProviderType === OPENSTACK && (
+        <Field
+          {...selectFieldBaseProps}
+          name="project"
+          label={__('Project')}
+          options={ospProjects}
+          option_key="id"
+          option_value="name"
+          disabled={!selectedProvider}
+        />
+      )}
     </Form>
   );
 };
