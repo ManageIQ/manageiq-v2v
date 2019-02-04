d04f56288b170a62fecbda2e31b2d6e776f4537b
Mike Turley <mturley@redhat.com>
2019-02-04 13:09:03 -0500

    Update existing tests

--- app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/__tests__/ConversionHostsSettings.test.js
+++ app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/__tests__/ConversionHostsSettings.test.js
@@ -3,12 +3,16 @@ import { shallow } from 'enzyme';
 import { Spinner } from 'patternfly-react';
 import ConversionHostsSettings from '../ConversionHostsSettings';
 import ConversionHostsEmptyState from '../components/ConversionHostsEmptyState';
+import ShowWizardEmptyState from '../../../../common/ShowWizardEmptyState/ShowWizardEmptyState';
 import ConversionHostsList from '../components/ConversionHostsList';
 
 describe('ConversionHostsSettings component', () => {
   const getBaseProps = () => ({
+    fetchProvidersAction: jest.fn(),
     fetchConversionHostsAction: jest.fn(),
     conversionHosts: [],
+    hasSufficientProviders: true,
+    isFetchingProviders: false,
     isFetchingConversionHosts: false,
     showConversionHostWizardAction: jest.fn(),
     hideConversionHostWizardAction: jest.fn(),
@@ -20,15 +24,29 @@ describe('ConversionHostsSettings component', () => {
     expect(component.find(Spinner).prop('loading')).toBeTruthy();
   });
 
-  it('renders the empty state when no conversion hosts are present', () => {
+  it('renders a spinner when fetching providers', () => {
+    const component = shallow(<ConversionHostsSettings {...getBaseProps()} isFetchingProviders />);
+    expect(component.find(Spinner).prop('loading')).toBeTruthy();
+  });
+
+  it('renders an empty state when insufficient providers are present', () => {
+    const component = shallow(<ConversionHostsSettings {...getBaseProps()} hasSufficientProviders={false} />);
+    expect(component.find(ShowWizardEmptyState)).toHaveLength(1);
+    expect(component.find(ConversionHostsEmptyState)).toHaveLength(0);
+    expect(component.find(ConversionHostsList)).toHaveLength(0);
+  });
+
+  it('renders an empty state when no conversion hosts are present', () => {
     const component = shallow(<ConversionHostsSettings {...getBaseProps()} />);
     expect(component.find(ConversionHostsEmptyState)).toHaveLength(1);
+    expect(component.find(ShowWizardEmptyState)).toHaveLength(0);
     expect(component.find(ConversionHostsList)).toHaveLength(0);
   });
 
   it('renders the list view when conversion hosts are present', () => {
     const component = shallow(<ConversionHostsSettings {...getBaseProps()} conversionHosts={[{ foo: 'bar' }]} />);
     expect(component.find(ConversionHostsList)).toHaveLength(1);
+    expect(component.find(ShowWizardEmptyState)).toHaveLength(0);
     expect(component.find(ConversionHostsEmptyState)).toHaveLength(0);
   });
 });
