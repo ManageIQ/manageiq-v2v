d90e8b34027f8437b3320e77a258bf55ea65f3b5
Milan Zazrivec <mzazrivec@redhat.com>
2019-02-05 16:20:58 +0100

    Conversion Hosts: list view phase 1
    - basic list view
    - pagination
    - filtering
    - sorting

--- app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostsList.js
+++ app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostsList.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Grid, ListView, Toolbar } from 'patternfly-react';
+import ListViewToolbar from '../../../../common/ListViewToolbar/ListViewToolbar';
 
 const ConversionHostsList = ({ conversionHosts }) => (
-  <React.Fragment>
-    <h2>TODO: render a list view here using the ListViewToolbar pattern we have elsewhere</h2>
-    {conversionHosts.map(conversionHost => (
-      <pre key={conversionHost.id}>{JSON.stringify(conversionHost)}</pre>
-    ))}
-  </React.Fragment>
+  <ListViewToolbar
+    filterTypes={ConversionHostsList.filterTypes}
+    sortFields={ConversionHostsList.sortFields}
+    listItems={conversionHosts}
+  >
+    {({
+      filteredSortedPaginatedListItems,
+      renderFilterControls,
+      renderSortControls,
+      renderActiveFilters,
+      renderPaginationRow
+    }) => (
+      <React.Fragment>
+        <Grid.Row>
+          <Toolbar>
+            {renderFilterControls()}
+            {renderSortControls()}
+            {renderActiveFilters(filteredSortedPaginatedListItems)}
+          </Toolbar>
+        </Grid.Row>
+        <div style={{ overflow: 'auto', paddingBottom: 300, height: '100%' }}>
+          <ListView className="conversion-hosts-list" id="conversion_hosts">
+            {filteredSortedPaginatedListItems.items.map((host, n) => (
+              <ListView.Item key={host.id} heading={host.name} stacked />
+            ))}
+          </ListView>
+          {renderPaginationRow(filteredSortedPaginatedListItems)}
+        </div>
+      </React.Fragment>
+    )}
+  </ListViewToolbar>
 );
 
 ConversionHostsList.propTypes = {
   conversionHosts: PropTypes.arrayOf(PropTypes.object)
 };
+
+ConversionHostsList.sortFields = [
+  {
+    id: 'name',
+    title: __('Name'),
+    isNumeric: false
+  }
+];
+
+ConversionHostsList.filterTypes = [
+  {
+    id: 'name',
+    title: __('Name'),
+    placeholder: __('Filter by Name'),
+    filterType: 'text'
+  }
+];
 
 export default ConversionHostsList;
