dd0e5e141ea7b04777d8a88c32084467013a1487
Mike Turley <mturley@redhat.com>
2019-02-13 18:12:29 -0500

    Upgrade to react-dropzone 8 to remove the need for a ref

--- app/javascript/react/screens/App/common/forms/TextFileInput/TextFileInput.js
+++ app/javascript/react/screens/App/common/forms/TextFileInput/TextFileInput.js
@@ -2,13 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Form, Button } from 'patternfly-react';
 import Dropzone from 'react-dropzone';
+import classNames from 'classnames';
 
 // TODO dropzone, browse button, callback on file loaded
 // TODO fill in file path, fill in file contents
-
-// Unfortunately, this is the recommended way to trigger the file dialog programmatically.
-// https://github.com/react-dropzone/react-dropzone/tree/master/examples/File%20Dialog
-let dropzoneRef;
 
 class TextFileInput extends React.Component {
   onFileDrop = files => {
@@ -24,29 +21,23 @@ class TextFileInput extends React.Component {
   render() {
     const { help, value, onChange } = this.props;
     return (
-      <Dropzone
-        className="text-file-input__dropzone"
-        activeClassName="active"
-        onDrop={this.onFileDrop}
-        disableClick
-        disablePreview
-        ref={node => {
-          dropzoneRef = node; // See comment at top, this is so we can call dropzoneRef.open below
-        }}
-      >
-        <React.Fragment>
-          <Form.InputGroup>
-            <Form.FormControl type="text" disabled />
-            <Form.InputGroup.Button>
-              <Button onClick={() => dropzoneRef.open()}>
-                {__('Browse')}
-                &hellip;
-              </Button>
-            </Form.InputGroup.Button>
-          </Form.InputGroup>
-          {help && <Form.HelpBlock>{help}</Form.HelpBlock>}
-          <Form.FormControl className="text-file-input__textarea" componentClass="textarea" />
-        </React.Fragment>
+      <Dropzone onDrop={this.onFileDrop} onClick={event => event.preventDefault()}>
+        {({ getRootProps, getInputProps, isDragActive, open }) => (
+          <div {...getRootProps()} className={classNames('text-file-input__dropzone', { active: isDragActive })}>
+            <input {...getInputProps()} />
+            <Form.InputGroup>
+              <Form.FormControl type="text" disabled />
+              <Form.InputGroup.Button>
+                <Button onClick={open}>
+                  {__('Browse')}
+                  &hellip;
+                </Button>
+              </Form.InputGroup.Button>
+            </Form.InputGroup>
+            {help && <Form.HelpBlock>{help}</Form.HelpBlock>}
+            <Form.FormControl className="text-file-input__textarea" componentClass="textarea" />
+          </div>
+        )}
       </Dropzone>
     );
   }
