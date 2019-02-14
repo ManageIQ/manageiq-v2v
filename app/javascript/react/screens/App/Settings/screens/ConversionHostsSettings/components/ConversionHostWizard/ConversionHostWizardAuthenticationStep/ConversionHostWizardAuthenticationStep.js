335d9d547c7ab780830abf2c6f8772fb80e15c6d
Mike Turley <mturley@redhat.com>
2019-02-14 14:56:16 -0500

    Add conditional openstack user field

--- app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostWizard/ConversionHostWizardAuthenticationStep/ConversionHostWizardAuthenticationStep.js
+++ app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostWizard/ConversionHostWizardAuthenticationStep/ConversionHostWizardAuthenticationStep.js
@@ -20,18 +20,32 @@ const ConversionHostWizardAuthenticationStep = ({ selectedProviderType }) => {
     sshKeyInfo = __('RHV-M deploys a common SSH public key on all hosts when configuring them. This allows commands and playbooks to be run from RHV-M. The associated private key is in the file /etc/pki/ovirt-engine/keys/engine_id_rsa on RHV-M.'); // prettier-ignore
   }
   if (selectedProviderType === OPENSTACK) {
-    sshKeyInfo = __('This is the private key file used to connect to the conversion host instance for cloud_user.');
+    sshKeyInfo = __('This is the private key file used to connect to the conversion host instance for cloud_user.'); // TODO do we want to use the actual entered username here?
   }
+
+  const fieldBaseProps = { labelWidth: 4, controlWidth: 7 };
+
   return (
     <Form className="form-horizontal">
+      {selectedProviderType === OPENSTACK && (
+        <Field
+          {...fieldBaseProps}
+          name="openstackUser"
+          component={FormField}
+          label={__('OpenStack User')}
+          type="text"
+          required
+          controlId="openstack-user-input"
+          validate={[required()]}
+        />
+      )}
       <Field
+        {...fieldBaseProps}
         name="sshKey"
         component={FormField}
         label={__('Conversion Host SSH key')}
         required
         info={sshKeyInfo}
-        labelWidth={4}
-        controlWidth={7}
         controlId="host-ssh-key-input"
         validate={[value => required()(value.body)]}
       >
@@ -51,6 +65,7 @@ export default reduxForm({
   form: stepIDs.authenticationStep,
   destroyOnUnmount: false,
   initialValues: {
+    openstackUser: 'cloud_user',
     sshKey: { filename: '', body: '' }
   }
 })(ConversionHostWizardAuthenticationStep);
