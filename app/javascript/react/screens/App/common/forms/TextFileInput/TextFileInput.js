f5370224a0cd8ed0061ac1f5f65db60a7b0f9192
Mike Turley <mturley@redhat.com>
2019-02-13 19:51:55 -0500

    Simplify TextFileInput as a function

--- app/javascript/react/screens/App/common/forms/TextFileInput/TextFileInput.js
+++ app/javascript/react/screens/App/common/forms/TextFileInput/TextFileInput.js
@@ -4,51 +4,45 @@ import { Form, Button } from 'patternfly-react';
 import Dropzone from 'react-dropzone';
 import classNames from 'classnames';
 
-class TextFileInput extends React.Component {
-  onFileDrop = files => {
+const TextFileInput = ({ value: { filename, body }, onChange, help }) => {
+  const readFile = fileHandle => {
+    const reader = new FileReader();
+    reader.onload = () => onChange({ filename: fileHandle.name, body: reader.result });
+    reader.readAsBinaryString(fileHandle);
+  };
+
+  const onFileDrop = files => {
     if (files && files.length > 0) {
-      this.handleFile(files[0]);
+      readFile(files[0]);
     }
   };
 
-  handleFile = fileHandle => {
-    if (fileHandle) {
-      const { onChange } = this.props;
-      const reader = new FileReader();
-      reader.onload = () => onChange({ filename: fileHandle.name, body: reader.result });
-      reader.readAsBinaryString(fileHandle);
-    }
-  };
-
-  render() {
-    const { help, value, onChange } = this.props;
-    return (
-      <Dropzone onDrop={this.onFileDrop} onClick={event => event.preventDefault()}>
-        {({ getRootProps, getInputProps, isDragActive, open }) => (
-          <div {...getRootProps()} className={classNames('text-file-input__dropzone', { active: isDragActive })}>
-            <input {...getInputProps()} />
-            <Form.InputGroup>
-              <Form.FormControl type="text" disabled value={value.filename} />
-              <Form.InputGroup.Button>
-                <Button onClick={open}>
-                  {__('Browse')}
-                  &hellip;
-                </Button>
-              </Form.InputGroup.Button>
-            </Form.InputGroup>
-            {help && <Form.HelpBlock>{help}</Form.HelpBlock>}
-            <Form.FormControl
-              className="text-file-input__textarea"
-              componentClass="textarea"
-              value={value.body}
-              onChange={event => onChange({ filename: '', body: event.target.value })}
-            />
-          </div>
-        )}
-      </Dropzone>
-    );
-  }
-}
+  return (
+    <Dropzone onDrop={onFileDrop} onClick={event => event.preventDefault()}>
+      {({ getRootProps, getInputProps, isDragActive, open }) => (
+        <div {...getRootProps()} className={classNames('text-file-input__dropzone', { active: isDragActive })}>
+          <input {...getInputProps()} />
+          <Form.InputGroup>
+            <Form.FormControl type="text" disabled value={filename} />
+            <Form.InputGroup.Button>
+              <Button onClick={open}>
+                {__('Browse')}
+                &hellip;
+              </Button>
+            </Form.InputGroup.Button>
+          </Form.InputGroup>
+          {help && <Form.HelpBlock>{help}</Form.HelpBlock>}
+          <Form.FormControl
+            className="text-file-input__textarea"
+            componentClass="textarea"
+            value={body}
+            onChange={event => onChange({ filename: '', body: event.target.value })}
+          />
+        </div>
+      )}
+    </Dropzone>
+  );
+};
 
 TextFileInput.propTypes = {
   help: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
