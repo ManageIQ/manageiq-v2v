27c7633a5b7605154b6951df6bc919f85cd7a080
Milan Zazrivec <mzazrivec@redhat.com>
2019-03-06 17:49:44 +0100

    Add new options for migration throttling

--- app/javascript/react/screens/App/Settings/screens/GeneralSettings/GeneralSettings.js
+++ app/javascript/react/screens/App/Settings/screens/GeneralSettings/GeneralSettings.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
-import { Form, Button, Spinner } from 'patternfly-react';
+import { Form, Button, Icon, OverlayTrigger, Popover, Spinner } from 'patternfly-react';
 import NumberInput from '../../../common/forms/NumberInput';
+// import TextInputWithCheckbox from '../../../common/forms/TextInputWithCheckbox'; // FIXME: uncomment once backend is ready
 
 export class GeneralSettings extends React.Component {
   componentDidMount() {
@@ -24,13 +25,46 @@ export class GeneralSettings extends React.Component {
       settingsForm.values &&
       Object.keys(savedSettings).some(key => savedSettings[key] !== settingsForm.values[key]);
 
+    // const inputEnabledFunction = value => value !== 'unlimited'; // FIXME: uncomment once backend is ready
+
     return (
       <Spinner loading={isFetchingServers || isFetchingSettings} style={{ marginTop: 15 }}>
         <div className="migration-settings">
-          <Form style={{ padding: '0 20px' }}>
+          <Form className="form-horizontal" style={{ padding: '0 20px' }}>
+            <div>
+              <h3>{__('Concurrent Migrations')}</h3>
+            </div>
             <Form.FormGroup>
-              <Form.ControlLabel>{__('Maximum concurrent migrations per conversion host')}</Form.ControlLabel>
-              <div style={{ width: 100 }}>
+              <Form.ControlLabel className="col-md-5">
+                <span className="pull-left">
+                  {__('Maximum concurrent migrations per conversion host')}
+                  <OverlayTrigger
+                    overlay={
+                      <Popover id="maximum_concurrect_migrations_per_provider_popover">
+                        {__(
+                          'For VDDK transformations the maximum concurrent migrations per conversion host is limited to 20. See the product documentation for more information.'
+                        )}
+                      </Popover>
+                    }
+                    placement="top"
+                    trigger={['hover']}
+                    delay={500}
+                    rootClose={false}
+                  >
+                    <Icon
+                      type="pf"
+                      name="info"
+                      size="md"
+                      style={{
+                        width: 'inherit',
+                        backgroundColor: 'transparent',
+                        padding: 10
+                      }}
+                    />
+                  </OverlayTrigger>
+                </span>
+              </Form.ControlLabel>
+              <div className="col-md-2">
                 <Field
                   id="max_concurrent_tasks_per_host"
                   name="max_concurrent_tasks_per_host"
@@ -41,10 +75,47 @@ export class GeneralSettings extends React.Component {
               </div>
             </Form.FormGroup>
             <Form.FormGroup>
+              <Form.ControlLabel className="col-md-5">
+                <div className="pull-left">{__('Maximum concurrent migrations per provider')}</div>
+              </Form.ControlLabel>
+              <div className="col-md-2">
+                <Field
+                  id="max_concurrent_tasks_per_ems"
+                  name="max_concurrent_tasks_per_ems"
+                  component={NumberInput}
+                  normalize={NumberInput.normalizeStringToInt}
+                  min={1}
+                />
+              </div>
+            </Form.FormGroup>
+{/* FIXME: uncomment once backend is ready
+            <Form.FormGroup />
+            <div>
+              <h3>{__('Resource Utilization Limits for Migrations')}</h3>
+            </div>
+            <Field
+              id="cpu_limit_per_host"
+              name="cpu_limit_per_host"
+              component={TextInputWithCheckbox}
+              normalize={TextInputWithCheckbox.normalizeStringToInt}
+              label={__('Max CPU utilization per conversion host')}
+              postfix="％"
+              inputEnabledFunction={inputEnabledFunction}
+            />
+            <Field
+              id="network_limit_per_host"
+              name="network_limit_per_host"
+              component={TextInputWithCheckbox}
+              normalize={TextInputWithCheckbox.normalizeStringToInt}
+              label={__('Total network throughput')}
+              postfix={__('MB/s')}
+              inputEnabledFunction={inputEnabledFunction}
+            />
+*/}
+            <Form.FormGroup className="col-md-1 pull-left" style={{ marginTop: '40px' }}>
               <Button bsStyle="primary" onClick={this.onApplyClick} disabled={!hasUnsavedChanges || isSavingSettings}>
                 {__('Apply')}
               </Button>
-              <br />
               {isSavingSettings && (
                 <div style={{ paddingTop: 10 }}>
                   <Spinner loading size="xs" inline />
