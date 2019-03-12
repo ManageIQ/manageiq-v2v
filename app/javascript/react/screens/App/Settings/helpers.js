9b2a950b3a2dbdb7a7bc44a5a2c7e38dbd004d43
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-03-12 17:18:12 -0400

    Merge pull request #897 from mzazrivec/add_new_options_for_migration_throttling
    Add new options for migration throttling
    
    (cherry picked from commit 5cee58ddbbbbe7e322363309a576d5729d58964b)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1693746

--- app/javascript/react/screens/App/Settings/helpers.js
+++ app/javascript/react/screens/App/Settings/helpers.js
@@ -1,13 +1,23 @@
 import { FINISHED, ERROR } from './screens/ConversionHostsSettings/ConversionHostsSettingsConstants';
 
 export const getFormValuesFromApiSettings = payload => ({
-  max_concurrent_tasks_per_host: payload.transformation.limits.max_concurrent_tasks_per_host
+  max_concurrent_tasks_per_host: payload.transformation.limits.max_concurrent_tasks_per_host,
+  max_concurrent_tasks_per_ems: payload.transformation.limits.max_concurrent_tasks_per_ems
+  /* FIXME: remove the comment once backend is ready
+  cpu_limit_per_host: payload.transformation.limits.cpu_limit_per_host,
+  network_limit_per_host: payload.transformation.limits.network_limit_per_host
+  */
 });
 
 export const getApiSettingsFromFormValues = values => ({
   transformation: {
     limits: {
-      max_concurrent_tasks_per_host: values.max_concurrent_tasks_per_host
+      max_concurrent_tasks_per_host: values.max_concurrent_tasks_per_host,
+      max_concurrent_tasks_per_ems: values.max_concurrent_tasks_per_ems
+      /* FIXME: remove the comment once backend is ready
+      cpu_limit_per_host: values.cpu_limit_per_host,
+      network_limit_per_host: values.network_limit_per_host
+      */
     }
   }
 });
--- app/javascript/react/screens/App/Settings/screens/GeneralSettings/GeneralSettings.js
+++ app/javascript/react/screens/App/Settings/screens/GeneralSettings/GeneralSettings.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
-import { Form, Button, Spinner } from 'patternfly-react';
+import { Form, Button, Icon, OverlayTrigger, Popover, Spinner } from 'patternfly-react';
 import NumberInput from '../../../common/forms/NumberInput';
+// import TextInputWithCheckbox from '../../../common/forms/TextInputWithCheckbox'; // FIXME: uncomment once backend is ready
+
+const FORM_NAME = 'settings';
 
 export class GeneralSettings extends React.Component {
   componentDidMount() {
@@ -16,6 +19,20 @@ export class GeneralSettings extends React.Component {
     patchSettingsAction(servers, settingsForm.values);
   };
 
+  enforceConstraintsOnChange = (event, newValue, prevValue, fieldChanging) => {
+    const {
+      settingsForm: {
+        values: { max_concurrent_tasks_per_host, max_concurrent_tasks_per_ems }
+      },
+      formChangeAction
+    } = this.props;
+    if (fieldChanging === 'max_concurrent_tasks_per_host' && newValue > max_concurrent_tasks_per_ems) {
+      formChangeAction(FORM_NAME, 'max_concurrent_tasks_per_ems', newValue);
+    } else if (fieldChanging === 'max_concurrent_tasks_per_ems' && newValue < max_concurrent_tasks_per_host) {
+      formChangeAction(FORM_NAME, 'max_concurrent_tasks_per_host', newValue);
+    }
+  };
+
   render() {
     const { isFetchingServers, isFetchingSettings, isSavingSettings, savedSettings, settingsForm } = this.props;
 
@@ -24,27 +41,99 @@ export class GeneralSettings extends React.Component {
       settingsForm.values &&
       Object.keys(savedSettings).some(key => savedSettings[key] !== settingsForm.values[key]);
 
+    // const inputEnabledFunction = value => value !== 'unlimited'; // FIXME: uncomment once backend is ready
+
     return (
       <Spinner loading={isFetchingServers || isFetchingSettings} style={{ marginTop: 15 }}>
         <div className="migration-settings">
-          <Form style={{ padding: '0 20px' }}>
+          <Form className="form-horizontal" style={{ padding: '0 20px' }}>
+            <div>
+              <h3>{__('Concurrent Migrations')}</h3>
+            </div>
             <Form.FormGroup>
-              <Form.ControlLabel>{__('Maximum concurrent migrations per conversion host')}</Form.ControlLabel>
-              <div style={{ width: 100 }}>
+              <Form.ControlLabel className="col-md-5">
+                <span className="pull-left">
+                  {__('Maximum concurrent migrations per conversion host')}
+                  <OverlayTrigger
+                    overlay={
+                      <Popover id="maximum_concurrect_migrations_per_provider_popover">
+                        {__(
+                          'For VDDK transformations the maximum concurrent migrations per conversion host is limited to 20. See the product documentation for more information.'
+                        )}
+                      </Popover>
+                    }
+                    placement="top"
+                    trigger={['hover']}
+                    delay={500}
+                    rootClose={false}
+                  >
+                    <Icon
+                      type="pf"
+                      name="info"
+                      size="md"
+                      style={{
+                        width: 'inherit',
+                        backgroundColor: 'transparent',
+                        padding: 10
+                      }}
+                    />
+                  </OverlayTrigger>
+                </span>
+              </Form.ControlLabel>
+              <div className="col-md-2">
                 <Field
                   id="max_concurrent_tasks_per_host"
                   name="max_concurrent_tasks_per_host"
                   component={NumberInput}
                   normalize={NumberInput.normalizeStringToInt}
                   min={1}
+                  onChange={this.enforceConstraintsOnChange}
                 />
               </div>
             </Form.FormGroup>
             <Form.FormGroup>
+              <Form.ControlLabel className="col-md-5">
+                <div className="pull-left">{__('Maximum concurrent migrations per provider')}</div>
+              </Form.ControlLabel>
+              <div className="col-md-2">
+                <Field
+                  id="max_concurrent_tasks_per_ems"
+                  name="max_concurrent_tasks_per_ems"
+                  component={NumberInput}
+                  normalize={NumberInput.normalizeStringToInt}
+                  min={1}
+                  onChange={this.enforceConstraintsOnChange}
+                />
+              </div>
+            </Form.FormGroup>
+            {/* FIXME: uncomment once backend is ready
+            <Form.FormGroup />
+            <div>
+              <h3>{__('Resource Utilization Limits for Migrations')}</h3>
+            </div>
+            <Field
+              id="cpu_limit_per_host"
+              name="cpu_limit_per_host"
+              component={TextInputWithCheckbox}
+              normalize={TextInputWithCheckbox.normalizeStringToInt}
+              label={__('Max CPU utilization per conversion host')}
+              postfix="％"
+              inputEnabledFunction={inputEnabledFunction}
+            />
+            <Field
+              id="network_limit_per_host"
+              name="network_limit_per_host"
+              component={TextInputWithCheckbox}
+              normalize={TextInputWithCheckbox.normalizeStringToInt}
+              label={__('Total network throughput')}
+              postfix={__('MB/s')}
+              inputEnabledFunction={inputEnabledFunction}
+            />
+*/}
+            <Form.FormGroup className="col-md-1 pull-left" style={{ marginTop: '40px' }}>
               <Button bsStyle="primary" onClick={this.onApplyClick} disabled={!hasUnsavedChanges || isSavingSettings}>
                 {__('Apply')}
               </Button>
-              <br />
               {isSavingSettings && (
                 <div style={{ paddingTop: 10 }}>
                   <Spinner loading size="xs" inline />
@@ -70,7 +159,8 @@ GeneralSettings.propTypes = {
   savedSettings: PropTypes.object,
   settingsForm: PropTypes.object,
   fetchServersUrl: PropTypes.string,
-  fetchSettingsUrl: PropTypes.string
+  fetchSettingsUrl: PropTypes.string,
+  formChangeAction: PropTypes.func
 };
 
 GeneralSettings.defaultProps = {
@@ -79,5 +169,5 @@ GeneralSettings.defaultProps = {
 };
 
 export default reduxForm({
-  form: 'settings'
+  form: FORM_NAME
 })(GeneralSettings);
