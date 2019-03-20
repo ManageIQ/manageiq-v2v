a7343ca4dd9fe652675a0aa46adb1e0865c6c49b
Milan Zazrivec <mzazrivec@redhat.com>
2019-03-20 10:49:01 +0100

    Update tests for new migration throttling inputs

--- app/javascript/react/screens/App/Settings/settings.fixures.js
+++ app/javascript/react/screens/App/Settings/settings.fixures.js
@@ -19,7 +19,9 @@ export const settings = Immutable({
   transformation: {
     limits: {
       max_concurrent_tasks_per_host: 10,
-      max_concurrent_tasks_per_ems: 10
+      max_concurrent_tasks_per_ems: 10,
+      cpu_limit_per_host: 10,
+      network_limit_per_host: 10
     }
   },
   otherSettings: {
