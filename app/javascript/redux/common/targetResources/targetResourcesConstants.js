0f6abc0be8e22eea95715f9f9825dac6aec3d0a1
Mike Turley <mturley@redhat.com>
2019-03-26 15:24:29 -0400

    Add Openstack filter on /api/cloud_tenants request in Mappings and Plans pages and in Mapping wizard.
    https://bugzilla.redhat.com/show_bug.cgi?id=1680521

--- app/javascript/redux/common/targetResources/targetResourcesConstants.js
+++ app/javascript/redux/common/targetResources/targetResourcesConstants.js
@@ -7,5 +7,6 @@ export const FETCH_TARGET_COMPUTE_URLS = {
     '/api/clusters?expand=resources' +
     '&attributes=ext_management_system.emstype,v_parent_datacenter,ext_management_system.name,hosts' +
     '&filter[]=ext_management_system.emstype=rhevm',
-  [OPENSTACK]: '/api/cloud_tenants?expand=resources&attributes=ext_management_system.name,ext_management_system.id,vms'
+  [OPENSTACK]:
+    '/api/cloud_tenants?expand=resources&filter[]=ext_management_system.type=ManageIQ::Providers::Openstack::CloudManager&attributes=ext_management_system.name,ext_management_system.id,vms'
 };
