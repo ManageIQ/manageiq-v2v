a13031f746997c73deb65045756b9edea02a4914
Mike Turley <mturley@redhat.com>
2019-05-06 16:43:17 -0400

    Add ConversionHostsSettings tests for polling and wizard rendering

--- app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/__tests__/ConversionHostsSettings.test.js
+++ app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/__tests__/ConversionHostsSettings.test.js
@@ -5,26 +5,27 @@ import ConversionHostsSettings from '../ConversionHostsSettings';
 import ConversionHostsEmptyState from '../components/ConversionHostsEmptyState';
 import ShowWizardEmptyState from '../../../../common/ShowWizardEmptyState/ShowWizardEmptyState';
 import ConversionHostsList from '../components/ConversionHostsList';
+import ConversionHostWizard from '../components/ConversionHostWizard';
+
+const getBaseProps = () => ({
+  deleteConversionHostAction: jest.fn(),
+  fetchProvidersAction: jest.fn(),
+  isFetchingProviders: false,
+  hasSufficientProviders: true,
+  fetchConversionHostsAction: jest.fn(() => Promise.resolve()),
+  fetchConversionHostTasksAction: jest.fn(() => Promise.resolve()),
+  combinedListItems: [],
+  showConversionHostWizardAction: jest.fn(),
+  conversionHostWizardMounted: false,
+  setHostToDeleteAction: jest.fn(),
+  showConversionHostDeleteModalAction: jest.fn(),
+  conversionHostDeleteModalVisible: false,
+  conversionHostToDelete: null,
+  isDeletingConversionHost: false,
+  hideConversionHostDeleteModalAction: jest.fn()
+});
 
 describe('ConversionHostsSettings component', () => {
-  const getBaseProps = () => ({
-    deleteConversionHostAction: jest.fn(),
-    fetchProvidersAction: jest.fn(),
-    isFetchingProviders: false,
-    hasSufficientProviders: true,
-    fetchConversionHostsAction: jest.fn(),
-    fetchConversionHostTasksAction: jest.fn(),
-    combinedListItems: [],
-    showConversionHostWizardAction: jest.fn(),
-    conversionHostWizardMounted: false,
-    setHostToDeleteAction: jest.fn(),
-    showConversionHostDeleteModalAction: jest.fn(),
-    conversionHostDeleteModalVisible: false,
-    conversionHostToDelete: null,
-    isDeletingConversionHost: false,
-    hideConversionHostDeleteModalAction: jest.fn()
-  });
-
   it('renders a spinner when fetching conversion hosts', () => {
     const component = shallow(<ConversionHostsSettings {...getBaseProps()} isFetchingConversionHosts />);
     expect(component.find(Spinner).prop('loading')).toBeTruthy();
@@ -40,6 +41,7 @@ describe('ConversionHostsSettings component', () => {
     expect(component.find(ShowWizardEmptyState)).toHaveLength(1);
     expect(component.find(ConversionHostsEmptyState)).toHaveLength(0);
     expect(component.find(ConversionHostsList)).toHaveLength(0);
+    expect(component.find(ConversionHostWizard)).toHaveLength(0);
   });
 
   it('renders an empty state when no conversion hosts are present', () => {
@@ -47,6 +49,7 @@ describe('ConversionHostsSettings component', () => {
     expect(component.find(ConversionHostsEmptyState)).toHaveLength(1);
     expect(component.find(ShowWizardEmptyState)).toHaveLength(0);
     expect(component.find(ConversionHostsList)).toHaveLength(0);
+    expect(component.find(ConversionHostWizard)).toHaveLength(0);
   });
 
   it('renders the list view when conversion hosts are present', () => {
@@ -54,7 +57,99 @@ describe('ConversionHostsSettings component', () => {
     expect(component.find(ConversionHostsList)).toHaveLength(1);
     expect(component.find(ShowWizardEmptyState)).toHaveLength(0);
     expect(component.find(ConversionHostsEmptyState)).toHaveLength(0);
+    expect(component.find(ConversionHostWizard)).toHaveLength(0);
+  });
+
+  it('renders the wizard when the wizard should be mounted', () => {
+    const component = shallow(<ConversionHostsSettings {...getBaseProps()} conversionHostWizardMounted />);
+    expect(component.find(ConversionHostWizard)).toHaveLength(1);
+  });
+
+  it('calls the show wizard action when clicking the configure link', () => {
+    const preventDefault = jest.fn();
+    const baseProps = getBaseProps();
+    const component = shallow(<ConversionHostsSettings {...baseProps} />);
+    component.find('.heading-with-link-container .pull-right a').simulate('click', { preventDefault });
+    expect(preventDefault).toHaveBeenCalledTimes(1);
+    expect(baseProps.showConversionHostWizardAction).toHaveBeenCalledTimes(1);
   });
 });
 
-// TODO finish coverage here
+describe('ConversionHostsSettings polling', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  const flushPromises = () => new Promise(setImmediate); // https://stackoverflow.com/a/51045733
+
+  it('fetches on mount, then polls every 15 seconds', async () => {
+    const baseProps = getBaseProps();
+    shallow(<ConversionHostsSettings {...baseProps} />);
+    await flushPromises();
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(1);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(1);
+    jest.advanceTimersByTime(15000);
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(2);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(2);
+    jest.advanceTimersByTime(15000);
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(3);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(3);
+  });
+
+  it('stops polling on unmount', async () => {
+    const baseProps = getBaseProps();
+    const component = shallow(<ConversionHostsSettings {...baseProps} />);
+    await flushPromises();
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(1);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(1);
+    jest.advanceTimersByTime(15000);
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(2);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(2);
+    component.unmount();
+    jest.advanceTimersByTime(15000);
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(2);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(2);
+    jest.advanceTimersByTime(15000);
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(2);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(2);
+  });
+
+  it('stops polling when a modal opens', async () => {
+    const baseProps = getBaseProps();
+    const component = shallow(<ConversionHostsSettings {...baseProps} />);
+    await flushPromises();
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(1);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(1);
+    jest.advanceTimersByTime(15000);
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(2);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(2);
+    component.setProps({ conversionHostWizardMounted: true });
+    jest.advanceTimersByTime(15000);
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(2);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(2);
+    jest.advanceTimersByTime(15000);
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(2);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(2);
+  });
+
+  it('restarts polling when a modal closes', async () => {
+    const baseProps = { ...getBaseProps(), conversionHostWizardMounted: true };
+    const component = shallow(<ConversionHostsSettings {...baseProps} />);
+    await flushPromises();
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(0);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(0);
+    jest.advanceTimersByTime(15000);
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(0);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(0);
+    component.setProps({ conversionHostWizardMounted: false });
+    await flushPromises();
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(1);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(1);
+    jest.advanceTimersByTime(15000);
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(2);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(2);
+    jest.advanceTimersByTime(15000);
+    expect(baseProps.fetchConversionHostsAction).toHaveBeenCalledTimes(3);
+    expect(baseProps.fetchConversionHostTasksAction).toHaveBeenCalledTimes(3);
+  });
+});
