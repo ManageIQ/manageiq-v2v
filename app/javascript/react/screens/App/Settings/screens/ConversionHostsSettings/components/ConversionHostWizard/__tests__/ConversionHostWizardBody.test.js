17688aa9e5fbb379c21292b469bf52841b72f6f9
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-05-17 16:38:08 -0400

    Add tests for ConversionHostWizardBody

--- app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostWizard/__tests__/ConversionHostWizardBody.test.js
+++ app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostWizard/__tests__/ConversionHostWizardBody.test.js
@@ -1,3 +1,43 @@
-// TODO add unit tests for ConversionHostWizardBody.js
+import React from 'react';
+import { shallow } from 'enzyme';
+import ConversionHostWizardBody from '../ConversionHostWizardBody';
 
-it('works', () => expect(true).toBe(true));
+describe('conversion host wizard body', () => {
+  const getBaseProps = () => ({
+    wizardSteps: [{ mock: 'step' }, { mock: 'step' }],
+    loaded: true,
+    activeStepIndex: 0,
+    activeStep: '1',
+    goToStep: jest.fn(),
+    disableNextStep: false,
+    plansBody: {}
+  });
+
+  it('renders correctly', () => {
+    const component = shallow(<ConversionHostWizardBody {...getBaseProps()} />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('performs deep props comparison on shouldComponentUpdate', () => {
+    const referencedObject = { someProperty: 'initialValue' };
+    const copyObject = { someProperty: 'initialValue' };
+    const changedObject = { someProperty: 'differentValue' };
+    const baseProps = {
+      ...getBaseProps(),
+      plansBody: referencedObject
+    };
+    const component = shallow(<ConversionHostWizardBody {...baseProps} />);
+    expect(
+      component.instance().shouldComponentUpdate({
+        ...baseProps,
+        plansBody: copyObject
+      })
+    ).toBeFalsy();
+    expect(
+      component.instance().shouldComponentUpdate({
+        ...baseProps,
+        plansBody: changedObject
+      })
+    ).toBeTruthy();
+  });
+});
