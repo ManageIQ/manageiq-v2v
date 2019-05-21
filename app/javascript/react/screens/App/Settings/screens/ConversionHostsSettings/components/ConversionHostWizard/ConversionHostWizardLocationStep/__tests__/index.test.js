4d202d11f0048d9d464ce3fb1937b6114174b601
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-05-21 19:05:44 -0400

    Add tests for ConversionHostWizardLocationStep

--- app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostWizard/ConversionHostWizardLocationStep/__tests__/index.test.js
+++ app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostWizard/ConversionHostWizardLocationStep/__tests__/index.test.js
@@ -1,3 +1,35 @@
-// TODO add unit tests for index.js
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { reducer as formReducer } from 'redux-form';
 
-it('works', () => expect(true).toBe(true));
+import { generateStore } from '../../../../../../../common/testReduxHelpers';
+import ConversionHostWizardLocationStep from '../ConversionHostWizardLocationStep';
+import ConversionHostWizardLocationStepContainer from '../index';
+import { RHV } from '../../../../../../../../../../common/constants';
+
+describe('ConversionHostWizardLocationStep integration test', () => {
+  const store = generateStore(
+    { form: formReducer },
+    {
+      form: { conversionHostWizardLocationStep: { values: { providerType: RHV, provider: '1' } } },
+      providers: { providers: [{ id: '1', name: 'Mock Provider', type: 'ManageIQ::Providers::Redhat::InfraManager' }] },
+      targetResources: { targetClusters: [], isFetchingTargetClusters: false }
+    }
+  );
+
+  const mountComponent = () =>
+    mount(
+      <Provider store={store}>
+        <ConversionHostWizardLocationStepContainer />
+      </Provider>
+    );
+
+  it('should mount ConversionHostWizardLocationStep with mapStateToProps reduced', () => {
+    const wrapper = mountComponent();
+
+    // query the unconnected component to assert reduced props
+    const component = wrapper.find(ConversionHostWizardLocationStep);
+    expect(component.props()).toMatchSnapshot();
+  });
+});
