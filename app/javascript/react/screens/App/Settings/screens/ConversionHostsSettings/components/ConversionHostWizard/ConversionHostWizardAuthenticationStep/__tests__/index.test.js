1dcf3f9c3ed08cb4e24dc44bd115711935b4e342
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-05-24 15:55:11 -0400

    Add a test for the unmounted required field problem (see PR #961)

--- app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostWizard/ConversionHostWizardAuthenticationStep/__tests__/index.test.js
+++ app/javascript/react/screens/App/Settings/screens/ConversionHostsSettings/components/ConversionHostWizard/ConversionHostWizardAuthenticationStep/__tests__/index.test.js
@@ -1,31 +1,74 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
-import { reducer as formReducer } from 'redux-form';
+import { reducer as formReducer, change } from 'redux-form';
 
 import { generateStore } from '../../../../../../../common/testReduxHelpers';
 import ConversionHostWizardAuthenticationStep from '../ConversionHostWizardAuthenticationStep';
 import ConversionHostWizardAuthenticationStepContainer from '../index';
-import { RHV } from '../../../../../../../../../../common/constants';
+import { OPENSTACK } from '../../../../../../../../../../common/constants';
+import { stepIDs } from '../../ConversionHostWizardConstants';
 
 describe('ConversionHostWizardAuthenticationStep integration test', () => {
-  const store = generateStore(
-    { form: formReducer },
-    { form: { conversionHostWizardLocationStep: { values: { providerType: RHV } } } }
-  );
-
-  const mountComponent = () =>
-    mount(
+  const mountComponentWithNewStore = () => {
+    const store = generateStore(
+      { form: formReducer },
+      { form: { conversionHostWizardLocationStep: { values: { providerType: OPENSTACK } } } }
+    );
+    const wrapper = mount(
       <Provider store={store}>
         <ConversionHostWizardAuthenticationStepContainer />
       </Provider>
     );
+    return { wrapper, store };
+  };
 
   it('should mount ConversionHostWizardAuthenticationStep with mapStateToProps reduced', () => {
-    const wrapper = mountComponent();
+    const { wrapper } = mountComponentWithNewStore();
 
     // query the unconnected component to assert reduced props
     const component = wrapper.find(ConversionHostWizardAuthenticationStep);
     expect(component.props()).toMatchSnapshot();
+  });
+
+  it('fails validation when there are empty required fields', () => {
+    const { store } = mountComponentWithNewStore();
+    expect(store.getState().form[stepIDs.authenticationStep].syncErrors).toBeTruthy();
+  });
+
+  // This test case is related to the bug fixed in https://github.com/ManageIQ/manageiq-v2v/pull/961
+  it('passes validation when required fields are unmounted while empty', () => {
+    const { store } = mountComponentWithNewStore();
+    const form = () => store.getState().form[stepIDs.authenticationStep];
+    const changeFormValue = (key, val) => store.dispatch(change(stepIDs.authenticationStep, key, val));
+
+    // Form starts with two empty required fields mounted.
+    expect(Object.keys(form().syncErrors)).toEqual(['conversionHostSshKey', 'transformationMethod']);
+
+    // Filling these in mounts another empty required field (vmwareSshKey).
+    changeFormValue('conversionHostSshKey', { filename: '', body: 'foo' });
+    changeFormValue('transformationMethod', 'SSH');
+    expect(Object.keys(form().syncErrors)).toEqual(['vmwareSshKey']);
+
+    // Filling in vmwareSshKey makes the form pass validation.
+    changeFormValue('vmwareSshKey', { filename: '', body: 'foo' });
+    expect(form().syncErrors).toBeFalsy();
+
+    // Changing the transformation method mounts an empty required field (vddkLibraryPath).
+    changeFormValue('transformationMethod', 'VDDK');
+    expect(Object.keys(form().syncErrors)).toEqual(['vddkLibraryPath']);
+
+    // Changing the transformation method back unmounts vddkLibraryPath while it's empty
+    // and remounts vmwareSshKey with its value from earlier, making the form pass validation.
+    changeFormValue('transformationMethod', 'SSH');
+    expect(form().syncErrors).toBeFalsy(); // <-------
+
+    // Turning on OpenStack TLS Certificates mounts an empty required field (openstackCaCerts)
+    changeFormValue('verifyOpenstackCerts', true);
+    expect(Object.keys(form().syncErrors)).toEqual(['openstackCaCerts']);
+
+    // Turning it off unmounts openstackCaCerts while it's empty.
+    changeFormValue('verifyOpenstackCerts', false);
+    expect(form().syncErrors).toBeFalsy(); // <-------
   });
 });
