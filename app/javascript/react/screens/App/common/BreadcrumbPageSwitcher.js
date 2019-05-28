312a539db3f7e57142e4c735db9bed3fcfa8e0b4
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-05-28 17:00:49 -0400

    Add BreadcrumbPageSwitcher for Plans, Mappings and Settings pages

---
+++ app/javascript/react/screens/App/common/BreadcrumbPageSwitcher.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { OverlayTrigger, Popover, Button, Icon, ListGroup, ListGroupItem } from 'patternfly-react';
+
+const BreadcrumbPageSwitcher = ({ activeHref }) => {
+  const SwitcherItem = ({ name, href }) => (
+    <ListGroupItem className={classNames('no-border', { active: href === activeHref })} href={href}>
+      {name}
+    </ListGroupItem>
+  );
+
+  const popoverContent = (
+    <ListGroup style={{ marginBottom: 0 }}>
+      <SwitcherItem name={__('Migration Plans')} href="#/plans" />
+      <SwitcherItem name={__('Infrastructure Mappings')} href="#/mappings" />
+      <SwitcherItem name={__('Migration Settings')} href="#/settings" />
+    </ListGroup>
+  );
+
+  return (
+    <OverlayTrigger
+      rootClose
+      trigger="click"
+      placement="bottom"
+      overlay={<Popover id="breadcrumb-switcher-popover">{popoverContent}</Popover>}
+    >
+      <Button style={{ marginTop: '-2px', marginBottom: '-4px', marginLeft: '15px' }}>
+        <Icon type="fa" name="exchange" />
+      </Button>
+    </OverlayTrigger>
+  );
+};
+
+BreadcrumbPageSwitcher.propTypes = {
+  activeHref: PropTypes.string
+};
+
+export default BreadcrumbPageSwitcher;
