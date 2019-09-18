366d5225d4ba2b9022e8cd15d2e9c73c3874526f
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-09-18 17:08:24 -0400

    Restructure MigrationsInProgressList container

--- app/javascript/react/screens/App/Overview/components/Migrations/MigrationsInProgressList.js
+++ app/javascript/react/screens/App/Overview/components/Migrations/MigrationsInProgressList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Grid, Spinner } from 'patternfly-react';
+import { Grid, Spinner } from 'patternfly-react';
 import ShowWizardEmptyState from '../../../common/ShowWizardEmptyState/ShowWizardEmptyState';
 import MigrationInProgressListItem from './MigrationInProgressListItem';
 
@@ -22,49 +22,42 @@ const MigrationsInProgressList = ({
   isCancellingPlanRequest,
   requestsProcessingCancellation
 }) => (
-  <div className="row-cards-pf">
-    <Grid.Row>
-      <Grid.Col
-        xs={12}
-        style={activeTransformationPlans.length > 0 && allRequestsWithTasks.length > 0 ? { marginTop: '20px' } : {}}
-        id="progress-bar-items"
-      >
-        <Card.HeightMatching selector={['.card-pf-match-height']}>
-          <Spinner loading={loading}>
-            {activeTransformationPlans.length > 0 && allRequestsWithTasks.length > 0 ? (
-              activeTransformationPlans.map(plan => (
-                <MigrationInProgressListItem
-                  plan={plan}
-                  serviceTemplatePlaybooks={serviceTemplatePlaybooks}
-                  allRequestsWithTasks={allRequestsWithTasks}
-                  reloadCard={reloadCard}
-                  key={plan.id}
-                  handleClick={redirectTo}
-                  fetchTransformationPlansAction={fetchTransformationPlansAction}
-                  fetchTransformationPlansUrl={fetchTransformationPlansUrl}
-                  isFetchingTransformationPlans={isFetchingTransformationPlans}
-                  isFetchingAllRequestsWithTasks={isFetchingAllRequestsWithTasks}
-                  acknowledgeDeniedPlanRequestAction={acknowledgeDeniedPlanRequestAction}
-                  isEditingPlanRequest={isEditingPlanRequest}
-                  setMigrationsFilterAction={setMigrationsFilterAction}
-                  cancelPlanRequestAction={cancelPlanRequestAction}
-                  isCancellingPlanRequest={isCancellingPlanRequest}
-                  requestsProcessingCancellation={requestsProcessingCancellation}
-                />
-              ))
-            ) : (
-              <ShowWizardEmptyState
-                title={__('No Migration Plans In Progress')}
-                iconType="pf"
-                iconName="info"
-                description={<span>{__('There are no existing migration plans in an In Progress state.')}</span>}
-              />
-            )}
-          </Spinner>
-        </Card.HeightMatching>
-      </Grid.Col>
-    </Grid.Row>
-  </div>
+  <React.Fragment>
+    <Grid.Col xs={12} id="progress-bar-items">
+      <Spinner loading={!!loading}>
+        {activeTransformationPlans.length > 0 && allRequestsWithTasks.length > 0 ? (
+          activeTransformationPlans.map(plan => (
+            <MigrationInProgressListItem
+              plan={plan}
+              serviceTemplatePlaybooks={serviceTemplatePlaybooks}
+              allRequestsWithTasks={allRequestsWithTasks}
+              reloadCard={reloadCard}
+              key={plan.id}
+              handleClick={redirectTo}
+              fetchTransformationPlansAction={fetchTransformationPlansAction}
+              fetchTransformationPlansUrl={fetchTransformationPlansUrl}
+              isFetchingTransformationPlans={isFetchingTransformationPlans}
+              isFetchingAllRequestsWithTasks={isFetchingAllRequestsWithTasks}
+              acknowledgeDeniedPlanRequestAction={acknowledgeDeniedPlanRequestAction}
+              isEditingPlanRequest={isEditingPlanRequest}
+              setMigrationsFilterAction={setMigrationsFilterAction}
+              cancelPlanRequestAction={cancelPlanRequestAction}
+              isCancellingPlanRequest={isCancellingPlanRequest}
+              requestsProcessingCancellation={requestsProcessingCancellation}
+            />
+          ))
+        ) : (
+          <ShowWizardEmptyState
+            title={__('No Migration Plans In Progress')}
+            iconType="pf"
+            iconName="info"
+            description={<span>{__('There are no existing migration plans in an In Progress state.')}</span>}
+          />
+        )}
+      </Spinner>
+    </Grid.Col>
+    {/* TODO: scheduling modal here? maybe lift that out of CompletedList? */}
+  </React.Fragment>
 );
 
 MigrationsInProgressList.propTypes = {
