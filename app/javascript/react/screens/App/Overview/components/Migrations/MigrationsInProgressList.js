0fba1bb3b432ce95f9b8c46a81505efea03773b3
Mike Turley <mike.turley@alum.cs.umass.edu>
2019-09-23 16:45:43 -0400

    Add ListViewToolbar sorting, filtering, and pagination controls

--- app/javascript/react/screens/App/Overview/components/Migrations/MigrationsInProgressList.js
+++ app/javascript/react/screens/App/Overview/components/Migrations/MigrationsInProgressList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Spinner } from 'patternfly-react';
+import { Grid, Spinner, Toolbar } from 'patternfly-react';
 import ShowWizardEmptyState from '../../../common/ShowWizardEmptyState/ShowWizardEmptyState';
 import MigrationInProgressListItem from './MigrationInProgressListItem';
 import ListViewTable from '../../../common/ListViewTable/ListViewTable';
+import ListViewToolbar from '../../../common/ListViewToolbar/ListViewToolbar';
+import { MIGRATIONS_FILTER_TYPES, MIGRATIONS_IN_PROGRESS_SORT_FIELDS } from './MigrationsConstants';
 
 const MigrationsInProgressList = ({
   activeTransformationPlans,
@@ -26,28 +28,52 @@ const MigrationsInProgressList = ({
   <Grid.Col xs={12} id="progress-bar-items">
     <Spinner loading={!!loading}>
       {activeTransformationPlans.length > 0 && allRequestsWithTasks.length > 0 ? (
-        <ListViewTable className="plans-in-progress-list" style={{ marginTop: 10 }}>
-          {activeTransformationPlans.map(plan => (
-            <MigrationInProgressListItem
-              plan={plan}
-              serviceTemplatePlaybooks={serviceTemplatePlaybooks}
-              allRequestsWithTasks={allRequestsWithTasks}
-              reloadCard={reloadCard}
-              key={plan.id}
-              handleClick={redirectTo}
-              fetchTransformationPlansAction={fetchTransformationPlansAction}
-              fetchTransformationPlansUrl={fetchTransformationPlansUrl}
-              isFetchingTransformationPlans={isFetchingTransformationPlans}
-              isFetchingAllRequestsWithTasks={isFetchingAllRequestsWithTasks}
-              acknowledgeDeniedPlanRequestAction={acknowledgeDeniedPlanRequestAction}
-              isEditingPlanRequest={isEditingPlanRequest}
-              setMigrationsFilterAction={setMigrationsFilterAction}
-              cancelPlanRequestAction={cancelPlanRequestAction}
-              isCancellingPlanRequest={isCancellingPlanRequest}
-              requestsProcessingCancellation={requestsProcessingCancellation}
-            />
-          ))}
-        </ListViewTable>
+        <ListViewToolbar
+          filterTypes={MIGRATIONS_FILTER_TYPES}
+          sortFields={MIGRATIONS_IN_PROGRESS_SORT_FIELDS}
+          listItems={activeTransformationPlans}
+        >
+          {({
+            filteredSortedPaginatedListItems,
+            renderFilterControls,
+            renderSortControls,
+            renderActiveFilters,
+            renderPaginationRow
+          }) => (
+            <React.Fragment>
+              <Grid.Row>
+                <Toolbar>
+                  {renderFilterControls()}
+                  {renderSortControls()}
+                  {renderActiveFilters(filteredSortedPaginatedListItems)}
+                </Toolbar>
+              </Grid.Row>
+              <ListViewTable className="plans-in-progress-list" style={{ marginTop: 10 }}>
+                {filteredSortedPaginatedListItems.items.map(plan => (
+                  <MigrationInProgressListItem
+                    plan={plan}
+                    serviceTemplatePlaybooks={serviceTemplatePlaybooks}
+                    allRequestsWithTasks={allRequestsWithTasks}
+                    reloadCard={reloadCard}
+                    key={plan.id}
+                    handleClick={redirectTo}
+                    fetchTransformationPlansAction={fetchTransformationPlansAction}
+                    fetchTransformationPlansUrl={fetchTransformationPlansUrl}
+                    isFetchingTransformationPlans={isFetchingTransformationPlans}
+                    isFetchingAllRequestsWithTasks={isFetchingAllRequestsWithTasks}
+                    acknowledgeDeniedPlanRequestAction={acknowledgeDeniedPlanRequestAction}
+                    isEditingPlanRequest={isEditingPlanRequest}
+                    setMigrationsFilterAction={setMigrationsFilterAction}
+                    cancelPlanRequestAction={cancelPlanRequestAction}
+                    isCancellingPlanRequest={isCancellingPlanRequest}
+                    requestsProcessingCancellation={requestsProcessingCancellation}
+                  />
+                ))}
+              </ListViewTable>
+              {renderPaginationRow(filteredSortedPaginatedListItems)}
+            </React.Fragment>
+          )}
+        </ListViewToolbar>
       ) : (
         <ShowWizardEmptyState
           title={__('No Migration Plans In Progress')}
