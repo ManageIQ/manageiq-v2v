51fb6cb3f920d266c77f748ee918663d95c6d472
Mike Turley <mike.turley@alum.cs.umass.edu>
2020-01-07 17:46:34 -0500

    Fix existing tests for PlanWizardScheduleStep

--- app/javascript/react/screens/App/Overview/screens/PlanWizard/components/PlanWizardScheduleStep/__tests__/PlanWizardScheduleStep.test.js
+++ app/javascript/react/screens/App/Overview/screens/PlanWizard/components/PlanWizardScheduleStep/__tests__/PlanWizardScheduleStep.test.js
@@ -1,49 +1,47 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { PlanWizardScheduleStep } from '../PlanWizardScheduleStep';
+import { OPENSTACK } from '../../../../../../../../../common/constants';
 
 describe('Plan wizard schedule step', () => {
+  const getBaseProps = () => ({
+    targetProvider: OPENSTACK,
+    migration_plan_type_radio: 'migration_type_cold',
+    migration_plan_choice_radio: 'migration_plan_now',
+    warmMigrationCompatibility: {
+      isFetchingTargetValidationData: false,
+      shouldEnableWarmMigration: true
+    },
+    showAlertAction: jest.fn()
+  });
+
   it('renders with a warning if migrating an OSP plan immediately', () => {
-    const component = shallow(
-      <PlanWizardScheduleStep
-        targetProvider="openstack"
-        migration_plan_type_radio="migration_type_cold"
-        migration_plan_choice_radio="migration_plan_now"
-        shouldEnableWarmMigration
-      />
-    );
+    const component = shallow(<PlanWizardScheduleStep {...getBaseProps()} />);
     expect(component).toMatchSnapshot();
   });
 
   it('renders with no warning if migrating an OSP plan later', () => {
     const component = shallow(
-      <PlanWizardScheduleStep
-        targetProvider="openstack"
-        migration_plan_type_radio="migration_type_cold"
-        migration_plan_choice_radio="migration_plan_later"
-        shouldEnableWarmMigration
-      />
+      <PlanWizardScheduleStep {...getBaseProps()} migration_plan_choice_radio="migration_plan_later" />
     );
     expect(component).toMatchSnapshot();
   });
 
   it('renders with a info text when warm migration selected', () => {
     const component = shallow(
-      <PlanWizardScheduleStep
-        targetProvider="openstack"
-        migration_plan_type_radio="migration_type_warm"
-        shouldEnableWarmMigration
-      />
+      <PlanWizardScheduleStep {...getBaseProps()} migration_plan_type_radio="migration_type_warm" />
     );
     expect(component).toMatchSnapshot();
   });
 
-  it('renders with a info text when warm migration is not available', () => {
+  it('renders with warm migration option disabled when it is not available', () => {
     const component = shallow(
       <PlanWizardScheduleStep
-        targetProvider="openstack"
-        migration_plan_type_radio="migration_type_cold"
-        shouldEnableWarmMigration={false}
+        {...getBaseProps()}
+        warmMigrationCompatibility={{
+          isFetchingTargetValidationData: false,
+          shouldEnableWarmMigration: false
+        }}
       />
     );
     expect(component).toMatchSnapshot();
