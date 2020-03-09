76cd4c3c2670aaf465ef543f5a169836044db798
Satoe Imaishi <simaishi@redhat.com>
2020-03-09 13:43:29 -0400

    Revert "Merge pull request #1112 from mturley/pr1085-ivanchuk-backport"
    This reverts commit 84a464decfc639314379b2222dc2c5cba91680a2, reversing
    changes made to d5b024bdf60ac65bdbf70c9d986c858a54bb9151.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1805911

--- app/javascript/common/constants.js
+++ app/javascript/common/constants.js
@@ -36,8 +36,6 @@ export const V2V_TARGET_PROVIDERS = [
 ];
 
 export const CONVERSION_HOST_TYPES = {
-  // With UCI support, all new conversion hosts must be VMs. But, RHV conversion hosts
-  // could have been configured pre-UCI on a Host, so those are still valid when checking for CHs.
-  [RHV]: ['ManageIQ::Providers::Redhat::InfraManager::Vm', 'ManageIQ::Providers::Redhat::InfraManager::Host'],
-  [OPENSTACK]: ['ManageIQ::Providers::Openstack::CloudManager::Vm']
+  [RHV]: 'ManageIQ::Providers::Redhat::InfraManager::Host',
+  [OPENSTACK]: 'ManageIQ::Providers::Openstack::CloudManager::Vm'
 };
