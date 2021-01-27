c05048b90b4a5ea64a77fc4ec14b747538fb5ed3
Adam Grare <agrare@redhat.com>
2021-01-27 12:16:44 -0500

    Use TEST_SUITE env var to run ruby/js specs

---
+++ bin/ci/before_install.sh
@@ -0,0 +1,5 @@
+if [ "$TEST_SUITE" = "javascript" ]
+then
+  source ~/.nvm/nvm.sh
+  nvm install 12
+fi
---
+++ bin/ci/install.sh
@@ -0,0 +1,6 @@
+if [ "$TEST_SUITE" = "javascript" ]
+then
+  npm install
+else
+  bin/setup
+fi
